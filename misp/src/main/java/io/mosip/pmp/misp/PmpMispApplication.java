a020fa87deb2ec6f7704fd98a50660ee3582ad43
Nagarjuna <Nagarjuna@Techno-232.technoforte.co.in>
2019-10-15 14:50:22 +0530

    MISP_Project_Structure_Updated

---
+++ misp/src/main/java/io/mosip/pmp/misp/PmpMispApplication.java
@@ -0,0 +1,14 @@
+package io.mosip.pmp.misp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class PmpMispApplication extends SpringBootServletInitializer {
+	
+	
+	public static void main(String[] args) {
+		SpringApplication.run(PmpMispApplication.class, args);
+	}
+} 
