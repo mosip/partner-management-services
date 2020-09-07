19c121881d4fc0b76d58bfee6a97f78d17fd79f6
Nagarjuna <Nagarjuna@host.docker.internal>
2020-09-07 12:52:44 +0530

    keycloak implemented

---
+++ partner/pmp-partner-service/src/main/java/io/mosip/pmp/authdevice/dto/UserRegistrationRequestDto.java
@@ -0,0 +1,41 @@
+package io.mosip.pmp.authdevice.dto;
+
+import java.time.LocalDate;
+
+import javax.validation.constraints.NotBlank;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserRegistrationRequestDto {
+	@NotBlank(message = "should not be null or empty")
+	private String userName;
+
+	private String firstName;
+
+	private String lastName;
+
+	@NotBlank(message = "should not be null or empty")
+	private String contactNo;
+
+	@NotBlank(message = "should not be null or empty")
+	private String emailID;
+
+	@JsonFormat(pattern = "yyyy-MM-dd")
+	private LocalDate dateOfBirth;
+
+	private String gender;
+
+	private String role;
+
+	@NotBlank(message = "should not be null or empty")
+	private String appId;
+
+	private String userPassword;
+}
--- partner/pmp-partner-service/src/main/java/io/mosip/pmp/partner/config/PartnerServiceConfig.java
+++ partner/pmp-partner-service/src/main/java/io/mosip/pmp/partner/config/PartnerServiceConfig.java
@@ -1,15 +1,44 @@
 package io.mosip.pmp.partner.config;
 
+import java.util.Collections;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.web.client.RestTemplate;
 
+import io.mosip.pmp.keycloak.impl.AccessTokenResponse;
+import io.mosip.pmp.keycloak.impl.MemoryCache;
+import io.mosip.pmp.keycloak.impl.RestInterceptor;
 import io.mosip.pmp.partner.util.RestUtil;
 
 @Configuration
 public class PartnerServiceConfig {
 
+	@Autowired
+	private RestInterceptor restInterceptor;
+	
 	@Bean
 	public RestUtil getRestUtil() {
 		return new RestUtil();
+	}
+	
+	@Primary
+	@Bean(name = "authRestTemplate")
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
+	}
+	
+	@Bean(name = "keycloakRestTemplate")
+	public RestTemplate getRestTemplate() {
+		RestTemplate restTemplate = new RestTemplate();
+		restTemplate.setInterceptors(Collections.singletonList(restInterceptor));
+		return restTemplate;
+	}
+	
+	@Bean
+	public MemoryCache<String, AccessTokenResponse> memoryCache() {
+		return new MemoryCache<>(1);
 	}
 }
