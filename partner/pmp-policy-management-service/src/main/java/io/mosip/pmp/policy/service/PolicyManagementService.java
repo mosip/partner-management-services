984508b8c9aac9673f37c5e0ecf974472f25ae82
Nagarjuna <Nagarjuna@host.docker.internal>
2020-10-01 16:35:17 +0530

    Device validation issue fix

--- partner/pmp-policy-management-service/src/main/java/io/mosip/pmp/policy/service/PolicyManagementService.java
+++ partner/pmp-policy-management-service/src/main/java/io/mosip/pmp/policy/service/PolicyManagementService.java
@@ -23,7 +23,7 @@ import com.fasterxml.jackson.core.JsonParseException;
 import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
-import io.mosip.kernel.auth.defaultadapter.model.AuthUserDetails;
+import io.mosip.kernel.auth.adapter.model.AuthUserDetails;
 import io.mosip.pmp.policy.constant.PolicyCommonConstants;
 import io.mosip.pmp.policy.dto.AllowedKycDto;
 import io.mosip.pmp.policy.dto.AuthPolicyDto;
