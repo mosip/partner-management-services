d4dfdaadf8553f002c766fb1f5ee43b9edc0baf4
Nagarjuna <Nagarjuna@Techno-296.technoforte.co.in>
2020-11-03 09:54:56 +0530

    Allowed partner types to add bio extractors can be configurable

--- partner/pmp-partner-service/src/main/java/io/mosip/pmp/partner/constant/PartnerExceptionConstants.java
+++ partner/pmp-partner-service/src/main/java/io/mosip/pmp/partner/constant/PartnerExceptionConstants.java
@@ -9,7 +9,7 @@ public enum PartnerExceptionConstants {
 	SERVER_ERROR("PMS_ATH_500", "Server error occured,Please check the logs "),
 	PARTNER_POLICY_MAPPING_NOT_EXISTS("PMS_PRT_061","Partner policy mapping not exists."),
 	PARTNER_API_KEY_REQUEST_APPROVED("PMS_PRT_062","Partner api key request is approved already. Cann't add extractors now."),
-	EXTRACTORS_ONLY_FOR_CREDENTIAL_PARTNER("PMS_PRT_063","Biometric extractors can be added only for Credential_Partner"),
+	EXTRACTORS_ONLY_FOR_CREDENTIAL_PARTNER("PMS_PRT_063","Biometric extractors can be added only for:"),
 	NO_DETAILS_FOUND("PMS_PRT_064","No details found");
 	/**
 	 * The error code.
