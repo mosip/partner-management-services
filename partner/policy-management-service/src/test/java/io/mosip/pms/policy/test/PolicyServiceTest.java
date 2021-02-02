1f8f4c25a64a183087b71de9ecf6302ef3c191d6
Nagarjuna K <nagarjunabtechece@gmail.com>
2021-02-02 09:30:44 +0530

    Test cases updated

--- partner/policy-management-service/src/test/java/io/mosip/pms/policy/test/PolicyServiceTest.java
+++ partner/policy-management-service/src/test/java/io/mosip/pms/policy/test/PolicyServiceTest.java
@@ -7,15 +7,19 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
 
+import io.mosip.pms.common.helper.FilterHelper;
+import io.mosip.pms.common.helper.SearchHelper;
 import io.mosip.pms.common.helper.WebSubPublisher;
+import io.mosip.pms.common.util.PageUtils;
 import io.mosip.pms.common.util.RestUtil;
+import io.mosip.pms.common.validator.FilterColumnValidator;
 
 /**
  * @author Nagarjuna
  *
  */
-@Import(value = {WebSubPublisher.class,RestUtil.class})
-@SpringBootApplication(scanBasePackages = { "io.mosip.pms.policy.*","io.mosip.pmp.common.*"})
+@Import(value = {WebSubPublisher.class,RestUtil.class,SearchHelper.class,FilterHelper.class,PageUtils.class,FilterColumnValidator.class,RestUtil.class})
+@SpringBootApplication(scanBasePackages = { "io.mosip.pms.policy.*","io.mosip.pms.common.*"})
 public class PolicyServiceTest {
 
 	/**
