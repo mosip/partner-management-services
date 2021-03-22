4fa63d69d216944621bd57f65cd49d8e62418617
rambhatt1591 <ram.bhatt@technoforte.co.in>
2021-03-22 13:10:04 +0530

    MOSIP-12271 mosip_pms Reverting is_deleted not null changes for 1.1.5

--- db_scripts/mosip_pms/ddl/pms-partner_contact.sql
+++ db_scripts/mosip_pms/ddl/pms-partner_contact.sql
@@ -8,7 +8,8 @@
 -- 
 -- Modified Date        Modified By         Comments / Remarks
 -- ------------------------------------------------------------------------------------------
--- Jan-2021		Ram Bhatt	    Set is_deleted flag to not null and default false 
+-- Jan-2021		Ram Bhatt	    Set is_deleted flag to not null and default false
+-- Mar-2021		Ram Bhatt	    Reverting is_deleted flag not null changes for 1.1.5   
 -- ------------------------------------------------------------------------------------------
 
 -- object: pms.partner_contact | type: TABLE --
@@ -24,7 +25,7 @@ CREATE TABLE pms.partner_contact(
 	cr_dtimes timestamp NOT NULL,
 	upd_by character varying(256),
 	upd_dtimes timestamp,
-	is_deleted boolean NOT NULL DEFAULT FALSE,
+	is_deleted boolean DEFAULT FALSE,
 	del_dtimes timestamp,
 	CONSTRAINT partcnt PRIMARY KEY (id),
 	CONSTRAINT uk_partcnt_idcnt UNIQUE (partner_id,contact_no),
