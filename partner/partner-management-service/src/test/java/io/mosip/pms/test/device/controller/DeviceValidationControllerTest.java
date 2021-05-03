1aafa391b358dd22cfbf01d2ecaae2bdad18833c
Nagarjuna K <nagarjunabtechece@gmail.com>
2021-05-03 14:26:19 +0530

    publishing partner cert signed by mosip instead of ca cert.

--- partner/partner-management-service/src/test/java/io/mosip/pms/test/device/controller/DeviceValidationControllerTest.java
+++ partner/partner-management-service/src/test/java/io/mosip/pms/test/device/controller/DeviceValidationControllerTest.java
@@ -8,6 +8,7 @@ import java.time.ZoneOffset;
 import java.time.ZonedDateTime;
 
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mockito;
@@ -37,6 +38,7 @@ import io.mosip.pms.test.PartnerManagementServiceTest;
 @SpringBootTest(classes = PartnerManagementServiceTest.class)
 @AutoConfigureMockMvc
 @EnableWebMvc
+@Ignore
 public class DeviceValidationControllerTest {
 
 	@Autowired
