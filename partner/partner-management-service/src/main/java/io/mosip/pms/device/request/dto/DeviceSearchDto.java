ccea4e17483b21b032a928df3a14fc460ccae530
Nagarjuna K <nagarjunabtechece@gmail.com>
2021-08-23 08:18:43 +0530

    updated comments

--- partner/partner-management-service/src/main/java/io/mosip/pms/device/request/dto/DeviceSearchDto.java
+++ partner/partner-management-service/src/main/java/io/mosip/pms/device/request/dto/DeviceSearchDto.java
@@ -1,6 +1,7 @@
 package io.mosip.pms.device.request.dto;
 
 import io.mosip.pms.common.dto.SearchDto;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
@@ -12,5 +13,6 @@ import lombok.NoArgsConstructor;
 @EqualsAndHashCode(callSuper=true)
 public class DeviceSearchDto extends SearchDto {
 
+	@ApiModelProperty(notes = "purpose is used to differentiate the device details.", required = true, example = "AUTH/REGISTRATION")
 	private String purpose;
 }
