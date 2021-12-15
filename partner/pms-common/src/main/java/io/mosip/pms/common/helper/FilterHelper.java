2424061ace2bfad6c64c2c6859948f41e05dfa5c
Nagarjuna K <nagarjunabtechece@gmail.com>
2021-12-15 09:59:47 +0530

    max columns increased

--- partner/pms-common/src/main/java/io/mosip/pms/common/helper/FilterHelper.java
+++ partner/pms-common/src/main/java/io/mosip/pms/common/helper/FilterHelper.java
@@ -59,7 +59,7 @@ public class FilterHelper  {
 	private static final String WILD_CARD_CHARACTER = "%";
 	private static final String FILTER_VALUE_EMPTY = "";
 
-	@Value("${mosip.pms.filtervalue.max_columns:100}")
+	@Value("${mosip.pms.filtervalue.max_columns:500}")
 	int filterValueMaxColumns;
 
 	@SuppressWarnings("unchecked")
