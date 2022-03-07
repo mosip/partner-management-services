f25a845c81f94858fa78ff98a265b4501713abc5
Nagarjuna K <nagarjunabtechece@gmail.com>
2022-03-07 08:12:17 +0530

    Status updated

--- db_release_scripts/mosip_pms/sql/1.2.0.1_pms-scripts_release.sql
+++ db_release_scripts/mosip_pms/sql/1.2.0.1_pms-scripts_release.sql
@@ -11,7 +11,7 @@
 \c mosip_pms sysadmin
 
 ALTER TABLE pms.ftp_chip_detail ADD COLUMN approval_status character varying(36);
-UPDATE pms.ftp_chip_detail SET approval_status='certpending' where certificate_alias is null;
+UPDATE pms.ftp_chip_detail SET approval_status='pending_cert_upload' where certificate_alias is null;
 UPDATE pms.ftp_chip_detail SET approval_status='pending_approval' where certificate_alias is not null and is_active =false;
 UPDATE pms.ftp_chip_detail SET approval_status='approved' where certificate_alias is not null and is_active = true;
 ALTER TABLE pms.ftp_chip_detail ALTER COLUMN approval_status SET NOT NULL;
