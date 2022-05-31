c027116a88f1333871c804705ab08f0d9009cf7e
nagarjunakuchi <nagarjunabtechece@gmail.com>
2022-05-31 07:03:19 +0530

    Added attributes

--- partner/partner-management-service/src/main/java/io/mosip/pms/partner/dto/UserRegistrationRequestDto.java
+++ partner/partner-management-service/src/main/java/io/mosip/pms/partner/dto/UserRegistrationRequestDto.java
@@ -1,10 +1,6 @@
 package io.mosip.pms.partner.dto;
 
-import java.time.LocalDate;
-
 import javax.validation.constraints.NotBlank;
-
-import com.fasterxml.jackson.annotation.JsonFormat;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -26,17 +22,13 @@ public class UserRegistrationRequestDto {
 	private String contactNo;
 
 	@NotBlank(message = "should not be null or empty")
-	private String emailID;
+	private String emailID;	
 
-	@JsonFormat(pattern = "yyyy-MM-dd")
-	private LocalDate dateOfBirth;
-
-	private String gender;
+	private String organizationName;
 
 	private String role;
-
-	@NotBlank(message = "should not be null or empty")
-	private String appId;
+	
+	private String partnerId;
 
 	private String userPassword;
 }
