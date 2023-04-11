c3a23a329f04efbdddb1c4b8961b541becc5aec9
GOKULRAJ136 <110164849+GOKULRAJ136@users.noreply.github.com>
2023-04-11 18:51:16 +0530

    MOSIP-25622 : Updated OIDC Client Details For Audit Purpose
    Signed-off-by: GOKULRAJ136 <110164849+GOKULRAJ136@users.noreply.github.com>

--- partner/partner-management-service/src/main/java/io/mosip/pms/oidc/client/controller/ClientManagementController.java
+++ partner/partner-management-service/src/main/java/io/mosip/pms/oidc/client/controller/ClientManagementController.java
@@ -1,5 +1,9 @@
 package io.mosip.pms.oidc.client.controller;
 
+import io.mosip.pms.device.util.AuditUtil;
+import io.mosip.pms.oidc.client.contant.ClientServiceAuditEnum;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 
 import io.mosip.pms.common.request.dto.RequestWrapper;
@@ -22,29 +26,55 @@ public class ClientManagementController {
 	@Autowired
 	ClientManagementService clientManagementService;
 
+	@Autowired
+	AuditUtil auditUtil;
+
+	String msg = "mosip.clientmanagement.clients.retrieve";
+
+	String version = "1.0";
+
 	@RequestMapping(value = "/oidc/client", method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
-	public ResponseWrapper<ClientDetailResponse> createClient(
+	public ResponseEntity<ResponseWrapper<ClientDetailResponse>> createClient(
 			@Valid @RequestBody RequestWrapper<ClientDetailCreateRequest> requestWrapper) throws Exception {
-		var clientRespDto = clientManagementService.createOIDCClient(requestWrapper.getRequest());
-		var response = new ResponseWrapper<ClientDetailResponse>();
-		response.setResponse(clientRespDto);
-		return response;
+		//var clientRespDto = clientManagementService.createOIDCClient(requestWrapper.getRequest());
+		ResponseWrapper<ClientDetailResponse> response = new ResponseWrapper<>();
+		//var response = new ResponseWrapper<ClientDetailResponse>();
+		ClientDetailResponse clientDetailResponse = null;
+		auditUtil.setAuditRequestDto(ClientServiceAuditEnum.CREATE_CLIENT,requestWrapper.getRequest().getName(),"clientID");
+		clientDetailResponse = clientManagementService.createOIDCClient(requestWrapper.getRequest());
+		//response.setResponse(clientRespDto);
+		response.setId(requestWrapper.getId());
+		response.setVersion(requestWrapper.getVersion());
+		response.setResponse(clientDetailResponse);
+		return new ResponseEntity<>(response, HttpStatus.OK);
 	}
 
 	@RequestMapping(value = "/oidc/client/{client_id}", method = RequestMethod.PUT, consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
-	public ResponseWrapper<ClientDetailResponse> updateClient(@PathVariable("client_id") String clientId,
+	public ResponseEntity<ResponseWrapper<ClientDetailResponse>> updateClient(@PathVariable("client_id") String clientId,
 			@Valid @RequestBody RequestWrapper<ClientDetailUpdateRequest> requestWrapper) throws Exception {
-		var clientRespDto = clientManagementService.updateOIDCClient(clientId, requestWrapper.getRequest());
-		var response = new ResponseWrapper<ClientDetailResponse>();
-		response.setResponse(clientRespDto);
-		return response;
+		//var clientRespDto = clientManagementService.updateOIDCClient(clientId, requestWrapper.getRequest());
+		ResponseWrapper<ClientDetailResponse> response = new ResponseWrapper<>();
+		//var response = new ResponseWrapper<ClientDetailResponse>();
+		ClientDetailResponse clientDetailResponse = null;
+		ClientDetailUpdateRequest updateRequest = requestWrapper.getRequest();
+		auditUtil.setAuditRequestDto(ClientServiceAuditEnum.UPDATE_CLIENT, clientId, "clientID");
+		clientDetailResponse = clientManagementService.updateOIDCClient(clientId, updateRequest);
+		response.setId(requestWrapper.getId());
+		response.setVersion(requestWrapper.getVersion());
+		response.setResponse(clientDetailResponse);
+		return new ResponseEntity<>(response, HttpStatus.OK);
 	}
 
 	@RequestMapping(value = "/oidc/client/{client_id}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
-	public ResponseWrapper<ClientDetail> getClient(@PathVariable("client_id") String clientId)
+	public ResponseEntity<ResponseWrapper<ClientDetail>> getClient(@PathVariable("client_id") String clientId)
 			throws Exception {
-		var response = new ResponseWrapper<ClientDetail>();
-		response.setResponse(clientManagementService.getClientDetails(clientId));
-		return response;
+		ResponseWrapper<ClientDetail> response = new ResponseWrapper<>();
+		ClientDetail clientDetail = null;
+		clientDetail = clientManagementService.getClientDetails(clientId);
+		//var response = new ResponseWrapper<ClientDetail>();
+		response.setId(msg);
+		response.setVersion(version);
+		response.setResponse(clientDetail);
+		return new ResponseEntity<>(response, HttpStatus.OK);
 	}
 }
