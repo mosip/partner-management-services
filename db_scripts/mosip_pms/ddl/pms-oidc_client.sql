a639fe453ab6c49897d74e943501e985d9c2b766
Rakshithb1 <79500257+Rakshithb1@users.noreply.github.com>
2023-11-24 16:47:13 +0530

    [MOSIP-29918] added db-test.yml and updated pms-oidc_client.sql  (#614)
    * [MOSIP-29918] added db-test.yml
    
    Signed-off-by: Rakshithb1 <rakshit.b@technoforte.co.in>
    
    * [MOSIP-29918] updated pms-oidc_client.sql
    
    Signed-off-by: Rakshithb1 <rakshit.b@technoforte.co.in>
    
    ---------
    
    Signed-off-by: Rakshithb1 <rakshit.b@technoforte.co.in>

--- db_scripts/mosip_pms/ddl/pms-oidc_client.sql
+++ db_scripts/mosip_pms/ddl/pms-oidc_client.sql
@@ -38,28 +38,28 @@ CREATE TABLE pms.oidc_client(
 	CONSTRAINT uq_oidc_client_public_key UNIQUE (public_key)
 );
 -- ddl-end --
-COMMENT ON TABLE oidc_client.id IS 'Client ID: Unique id assigned to registered OIDC client.';
+COMMENT ON TABLE pms.oidc_client IS 'Client ID: Unique id assigned to registered OIDC client.';
 -- ddl-end --
-COMMENT ON COLUMN oidc_client.name IS 'Client Name: Registered name of OIDC client.';
+COMMENT ON COLUMN pms.oidc_client.name IS 'Client Name: Registered name of OIDC client.';
 -- ddl-end --
-COMMENT ON COLUMN oidc_client.logo_uri IS 'Client Logo URL: Client logo to be displayed on IDP UI.';
+COMMENT ON COLUMN pms.oidc_client.logo_uri IS 'Client Logo URL: Client logo to be displayed on IDP UI.';
 -- ddl-end --
-COMMENT ON COLUMN oidc_client.redirect_uris IS 'Recirect URLS: Comma separated list of client redirect URLs.';
+COMMENT ON COLUMN pms.oidc_client.redirect_uris IS 'Recirect URLS: Comma separated list of client redirect URLs.';
 -- ddl-end --
-COMMENT ON COLUMN oidc_client.rp_id IS 'Relaying Party Id: Id of the partner id who has created this OIDC client.';
+COMMENT ON COLUMN pms.oidc_client.rp_id IS 'Relaying Party Id: Id of the partner id who has created this OIDC client.';
 -- ddl-end --
-COMMENT ON COLUMN oidc_client.policy_id IS 'Policy Id: Id of the policy ';
+COMMENT ON COLUMN pms.oidc_client.policy_id IS 'Policy Id: Id of the policy ';
 -- ddl-end --
-COMMENT ON COLUMN oidc_client.status IS 'Client status: Allowed values - ACTIVE / INACTIVE.';
+COMMENT ON COLUMN pms.oidc_client.status IS 'Client status: Allowed values - ACTIVE / INACTIVE.';
 -- ddl-end --
-COMMENT ON COLUMN oidc_client.public_key IS 'Public key: JWK data.';
+COMMENT ON COLUMN pms.oidc_client.public_key IS 'Public key: JWK data.';
 -- ddl-end --
-COMMENT ON COLUMN oidc_client.grant_types IS 'Grant Types: Allowed grant types for the client.';
+COMMENT ON COLUMN pms.oidc_client.grant_types IS 'Grant Types: Allowed grant types for the client.';
 -- ddl-end --
-COMMENT ON COLUMN oidc_client.auth_methods IS 'Client Auth methods: Allowed token endpoint authentication methods.';
+COMMENT ON COLUMN pms.oidc_client.auth_methods IS 'Client Auth methods: Allowed token endpoint authentication methods.';
 -- ddl-end --
-COMMENT ON COLUMN oidc_client.claims IS 'Requested Claims: claims json as per policy defined for relaying party.';
+COMMENT ON COLUMN pms.oidc_client.claims IS 'Requested Claims: claims json as per policy defined for relaying party.';
 -- ddl-end --
-COMMENT ON COLUMN oidc_client.acr_values IS 'Allowed Authentication context References(acr) json';
+COMMENT ON COLUMN pms.oidc_client.acr_values IS 'Allowed Authentication context References(acr) json';
 -- ddl-end --
 
