a3bd0b8ecbb07d089342e5dbfb36837cab9afb34
Balaji <74903654+balaji-alluru@users.noreply.github.com>
2024-04-15 12:56:59 +0530

    MOSIP-32300
    Signed-off-by: Balaji <74903654+balaji-alluru@users.noreply.github.com>

---
+++ db_upgrade_scripts/mosip_pms/sql/1.2.0.1_to_1.2.1.0_rollback.sql
@@ -0,0 +1,7 @@
+\c mosip_pms
+
+ALTER TABLE pms.partner DROP COLUMN logo_url;
+ALTER TABLE pms.partner DROP COLUMN addl_info;
+
+ALTER TABLE pms.partner_h DROP COLUMN logo_url;
+ALTER TABLE pms.partner_h DROP COLUMN addl_info;
