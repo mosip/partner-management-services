c54e410ca21b91dd131b62c8e2cbb36a1511b94e
kameshsr <kameshsr1338@gmail.com>
2024-05-07 11:46:59 +0530

    MOSIP-32842 Change javax.servelt. to jakarta.servlet.
    Signed-off-by: kameshsr <kameshsr1338@gmail.com>

--- partner/partner-management-service/src/main/java/io/mosip/pms/exception/PartnerServiceResponseExceptionHandler.java
+++ partner/partner-management-service/src/main/java/io/mosip/pms/exception/PartnerServiceResponseExceptionHandler.java
@@ -8,7 +8,7 @@ import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 
-import javax.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
--- partner/policy-management-service/src/main/java/io/mosip/pms/policy/errorMessages/ApiExceptionHandler.java
+++ partner/policy-management-service/src/main/java/io/mosip/pms/policy/errorMessages/ApiExceptionHandler.java
@@ -6,7 +6,7 @@ import java.time.ZoneId;
 import java.util.ArrayList;
 import java.util.List;
 
-import javax.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
