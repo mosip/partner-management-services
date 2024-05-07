9e1a34cbfac07045e4bca3cc5760b94e75ba4f04
kameshsr <kameshsr1338@gmail.com>
2024-05-07 18:57:06 +0530

    MOSIP-32842 Removed version
    Signed-off-by: kameshsr <kameshsr1338@gmail.com>

--- partner/policy-management-service/src/test/java/io/mosip/pms/policy/test/controller/PolicyManagementControllerTest.java
+++ partner/policy-management-service/src/test/java/io/mosip/pms/policy/test/controller/PolicyManagementControllerTest.java
@@ -31,7 +31,6 @@ import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -59,7 +58,6 @@ import io.mosip.pms.policy.errorMessages.PolicyManagementServiceException;
 import io.mosip.pms.policy.service.PolicyManagementService;
 import io.mosip.pms.policy.test.PolicyServiceTest;
 import io.mosip.pms.policy.util.AuditUtil;
-import org.mockito.Mockito;
 
 /**
  * @author Nagarjuna Kuchi
