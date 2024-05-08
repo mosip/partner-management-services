1febe64a2af4643a6be327da97ad995b444c6efe
kameshsr <kameshsr1338@gmail.com>
2024-05-08 13:33:00 +0530

    MOSIP-32842 Added correcte package
    Signed-off-by: kameshsr <kameshsr1338@gmail.com>

--- partner/partner-management-service/src/main/java/io/mosip/pms/service/PartnerManagementService.java
+++ partner/partner-management-service/src/main/java/io/mosip/pms/service/PartnerManagementService.java
@@ -1,7 +1,11 @@
 package io.mosip.pms.service;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Import;
 
@@ -14,7 +18,9 @@ import io.mosip.pms.common.validator.FilterColumnValidator;
 
 @SpringBootApplication
 @Import(value = {WebSubPublisher.class,RestUtil.class,FilterColumnValidator.class,FilterHelper.class,SearchHelper.class,PageUtils.class})
-@ComponentScan(basePackages = {"io.mosip.pms.*", "${mosip.auth.adapter.impl.basepackage}"})
+@ComponentScan(basePackages = {"io.mosip.*", "${mosip.auth.adapter.impl.basepackage}"})
+@EnableAutoConfiguration(exclude = { DataSourceAutoConfiguration.class,
+		DataSourceTransactionManagerAutoConfiguration.class, HibernateJpaAutoConfiguration.class })
 public class PartnerManagementService {
 
 	public static void main(String[] args) {
