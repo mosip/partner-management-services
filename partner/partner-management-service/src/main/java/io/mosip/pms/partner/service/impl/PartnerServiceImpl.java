ab4022a1eab3fdff3d0d6d9795ef62f591ac87ae
Sudeep7353 <103890430+Sudeep7353@users.noreply.github.com>
2024-05-09 16:34:19 +0530

    MOSIP:32732 - API To fetch list of available partner certificates based on partner type. (#664)
    * MOSIP:32732 - API To fetch list of available partner certificates based on partner type.
    
    Signed-off-by: sudeep <sudeep.j7353@gmail.com>
    
    * MOSIP:32732 - API To fetch list of available partner certificates based on partner type.
    
    Signed-off-by: sudeep <sudeep.j7353@gmail.com>
    
    * MOSIP:32732 - API To fetch list of available partner certificates based on partner type.
    
    Signed-off-by: sudeep <sudeep.j7353@gmail.com>
    
    ---------
    
    Signed-off-by: sudeep <sudeep.j7353@gmail.com>

--- partner/partner-management-service/src/main/java/io/mosip/pms/partner/service/impl/PartnerServiceImpl.java
+++ partner/partner-management-service/src/main/java/io/mosip/pms/partner/service/impl/PartnerServiceImpl.java
@@ -5,8 +5,11 @@ import java.io.IOException;
 import java.security.cert.Certificate;
 import java.security.cert.CertificateException;
 import java.security.cert.CertificateFactory;
+import java.security.cert.X509Certificate;
 import java.sql.Timestamp;
 import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Base64;
@@ -19,6 +22,9 @@ import java.util.Objects;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
+import io.mosip.kernel.core.authmanager.authadapter.model.AuthUserDetails;
+import io.mosip.pms.common.response.dto.ResponseWrapper;
+import io.mosip.pms.partner.dto.CertificateDto;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
@@ -28,6 +34,7 @@ import org.springframework.core.env.Environment;
 import org.springframework.core.io.ByteArrayResource;
 import org.springframework.data.domain.Page;
 import org.springframework.http.MediaType;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.LinkedMultiValueMap;
@@ -137,6 +144,8 @@ public class PartnerServiceImpl implements PartnerService {
 	private static final String END_CERTIFICATE = "-----END CERTIFICATE-----";
 
 	private final static String LINE_SEPARATOR = "\n";
+
+	public static final String BLANK_STRING="";
 
 	@Autowired
 	PartnerServiceRepository partnerRepository;
@@ -1619,5 +1628,73 @@ public class PartnerServiceImpl implements PartnerService {
 		updateRequest.setAdditionalInfo(null);
 		updateRequest.setLogoUrl(null);	
 		return updatePartnerDetails(updateRequest, partnerId);
+	}
+
+	@Override
+	public List<CertificateDto> getAllCertificateDetails() {
+		List<CertificateDto> certificateDtoList = new ArrayList<>();
+		try {
+			String userId = getUserId();
+			List<Partner> partnerList = partnerRepository.findByUserId(userId);
+			if (!partnerList.isEmpty()) {
+				for (Partner partner : partnerList) {
+					CertificateDto certificateDto = new CertificateDto();
+					try {
+						if (Objects.nonNull(partner.getId()) && !partner.getId().equals(BLANK_STRING)) {
+							LOGGER.info("Partner Id is null or empty for user id : " + userId);
+							throw new PartnerServiceException(ErrorCode.PARTNER_ID_NOT_EXISTS.getErrorCode(),
+									ErrorCode.PARTNER_ID_NOT_EXISTS.getErrorMessage());
+						}
+						PartnerCertDownloadRequestDto requestDto = new PartnerCertDownloadRequestDto();
+						requestDto.setPartnerId(partner.getId());
+						PartnerCertDownloadResponeDto partnerCertDownloadResponeDto = getPartnerCertificate(requestDto);
+						String certificateData = partnerCertDownloadResponeDto.getCertificateData();
+						certificateData = certificateData.replaceAll(BEGIN_CERTIFICATE, "")
+								.replaceAll(END_CERTIFICATE, "")
+								.replaceAll("\n", "");
+
+						byte[] decodedCertificate = Base64.getDecoder().decode(certificateData);
+
+						CertificateFactory certificateFactory = CertificateFactory.getInstance("X.509");
+						X509Certificate cert = (X509Certificate) certificateFactory.generateCertificate(new ByteArrayInputStream(decodedCertificate));
+
+						certificateDto.setIsCertificateAvailable(true);
+						certificateDto.setCertificateUploadDate(cert.getNotBefore());
+						certificateDto.setCertificateExpiryDate(cert.getNotAfter());
+						certificateDto.setPartnerId(partner.getId());
+						certificateDto.setPartnerType(partner.getPartnerTypeCode());
+					} catch (PartnerServiceException ex) {
+						LOGGER.info("Could not fetch partner certificate :" + ex.getMessage());
+						certificateDto.setIsCertificateAvailable(false);
+						certificateDto.setPartnerId(partner.getId());
+						certificateDto.setPartnerType(partner.getPartnerTypeCode());
+					}
+					certificateDtoList.add(certificateDto);
+				}
+			} else {
+				LOGGER.info("sessionId", "idType", "id", "User id does not exists.");
+				throw new PartnerServiceException(ErrorCode.USER_ID_NOT_EXISTS.getErrorCode(),
+						ErrorCode.USER_ID_NOT_EXISTS.getErrorMessage());
+			}
+		} catch (PartnerServiceException ex) {
+			LOGGER.info("sessionId", "idType", "id", "In getAllCertificateDetails method of PartnerServiceImpl - " + ex.getMessage());
+			throw ex;
+		} catch (Exception ex) {
+			LOGGER.debug("sessionId", "idType", "id", ex.getStackTrace());
+			LOGGER.error("sessionId", "idType", "id",
+					"In getAllCertificateDetails method of PartnerServiceImpl - " + ex.getMessage());
+			throw new PartnerServiceException(ErrorCode.PARTNER_CERTIFICATES_FETCH_ERROR.getErrorCode(),
+					ErrorCode.PARTNER_CERTIFICATES_FETCH_ERROR.getErrorMessage());
+		}
+		return certificateDtoList;
+	}
+
+	private AuthUserDetails authUserDetails() {
+		return (AuthUserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+	}
+
+	private String getUserId() {
+		String userId = authUserDetails().getUserId();
+		return userId;
 	}
 }
