a610a7fe0de00dabcf816d06f6c2b844864d5737
kameshsr <kameshsr1338@gmail.com>
2024-05-13 11:31:02 +0530

    MOSIP-32842 Fix startup issue
    Signed-off-by: kameshsr <kameshsr1338@gmail.com>

--- partner/pms-common/src/main/java/io/mosip/pms/common/config/AntPathMatcherConfig.java
+++ partner/pms-common/src/main/java/io/mosip/pms/common/config/AntPathMatcherConfig.java
@@ -2,14 +2,12 @@ package io.mosip.pms.common.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Lazy;
 import org.springframework.util.AntPathMatcher;
 
 @Configuration
 public class AntPathMatcherConfig {
 	@Bean
-	@Lazy
-	public AntPathMatcher antPathMatcher(AntPathMatcher antPathMatcher) {
+	public AntPathMatcher antPathMatcher() {
 		return new AntPathMatcher();
 	}
 }
