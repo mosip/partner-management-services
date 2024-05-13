e7a4be6ea5907bf5657b42f4d99b6fe0424c0367
kameshsr <kameshsr1338@gmail.com>
2024-05-13 13:58:29 +0530

    MOSIP-32842 Added exclusion to authorizedrolesdto
    Signed-off-by: kameshsr <kameshsr1338@gmail.com>

--- partner/partner-management-service/src/main/java/io/mosip/pms/service/PartnerManagementService.java
+++ partner/partner-management-service/src/main/java/io/mosip/pms/service/PartnerManagementService.java
@@ -8,6 +8,7 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
 import org.springframework.context.annotation.Import;
 
 import io.mosip.pms.common.helper.FilterHelper;
@@ -17,9 +18,10 @@ import io.mosip.pms.common.util.PageUtils;
 import io.mosip.pms.common.util.RestUtil;
 import io.mosip.pms.common.validator.FilterColumnValidator;
 
-@SpringBootApplication(exclude= {AuthorizedRolesDto.class})
+@SpringBootApplication
 @Import(value = {WebSubPublisher.class,RestUtil.class,FilterColumnValidator.class,FilterHelper.class,SearchHelper.class,PageUtils.class})
-@ComponentScan(basePackages = {"io.mosip.*", "${mosip.auth.adapter.impl.basepackage}"})
+@ComponentScan(basePackages = {"io.mosip.*", "${mosip.auth.adapter.impl.basepackage}"},excludeFilters = {
+		@ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, value = AuthorizedRolesDto.class) })
 @EnableAutoConfiguration(exclude = { DataSourceAutoConfiguration.class,
 		DataSourceTransactionManagerAutoConfiguration.class, HibernateJpaAutoConfiguration.class })
 public class PartnerManagementService {
