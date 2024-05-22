49eb1858e20ae5e1278bfb12778d5d8000d29bc5
SwethaKrish4 <110164892+SwethaKrish4@users.noreply.github.com>
2024-05-22 14:24:43 +0530

    MOSIP-32692: API to get all policies request (#677)
    Signed-off-by: SwethaKrish4 <110164892+SwethaKrish4@users.noreply.github.com>

--- partner/partner-management-service/src/main/java/io/mosip/pms/partner/service/impl/MultiPartnerServiceImpl.java
+++ partner/partner-management-service/src/main/java/io/mosip/pms/partner/service/impl/MultiPartnerServiceImpl.java
@@ -2,11 +2,16 @@ package io.mosip.pms.partner.service.impl;
 
 import io.mosip.kernel.core.authmanager.authadapter.model.AuthUserDetails;
 import io.mosip.kernel.core.logger.spi.Logger;
+import io.mosip.pms.common.entity.AuthPolicy;
 import io.mosip.pms.common.entity.Partner;
+import io.mosip.pms.common.entity.PartnerPolicyRequest;
+import io.mosip.pms.common.repository.AuthPolicyRepository;
 import io.mosip.pms.common.repository.PartnerServiceRepository;
+import io.mosip.pms.common.repository.PolicyGroupRepository;
 import io.mosip.pms.common.util.PMSLogger;
 import io.mosip.pms.partner.constant.ErrorCode;
 import io.mosip.pms.partner.dto.CertificateDto;
+import io.mosip.pms.partner.dto.PolicyDto;
 import io.mosip.pms.partner.exception.PartnerServiceException;
 import io.mosip.pms.partner.request.dto.PartnerCertDownloadRequestDto;
 import io.mosip.pms.partner.response.dto.PartnerCertDownloadResponeDto;
@@ -32,6 +37,12 @@ public class MultiPartnerServiceImpl implements MultiPartnerService {
     private static final String END_CERTIFICATE = "-----END CERTIFICATE-----";
     @Autowired
     PartnerServiceRepository partnerRepository;
+
+    @Autowired
+    PolicyGroupRepository policyGroupRepository;
+
+    @Autowired
+    AuthPolicyRepository authPolicyRepository;
 
     @Autowired
     PartnerServiceImpl partnerServiceImpl;
@@ -94,6 +105,64 @@ public class MultiPartnerServiceImpl implements MultiPartnerService {
                     ErrorCode.PARTNER_CERTIFICATES_FETCH_ERROR.getErrorMessage());
         }
         return certificateDtoList;
+    }
+
+    @Override
+    public List<PolicyDto> getAllPolicies() {
+        List<PolicyDto> policyDtoList = new ArrayList<>();
+        try {
+            String userId = getUserId();
+            List<Partner> partnerList = partnerRepository.findByUserId(userId);
+            if (!partnerList.isEmpty()) {
+                for (Partner partner : partnerList) {
+                    try {
+                        if (Objects.isNull(partner.getId()) || partner.getId().equals(BLANK_STRING)) {
+                            LOGGER.info("Partner Id is null or empty for user id : " + userId);
+                            throw new PartnerServiceException(ErrorCode.PARTNER_ID_NOT_EXISTS.getErrorCode(),
+                                    ErrorCode.PARTNER_ID_NOT_EXISTS.getErrorMessage());
+                        }
+                        String policyGroupName = policyGroupRepository.findPolicyGroupNameById(partner.getPolicyGroupId());
+                        if (Objects.isNull(policyGroupName) || policyGroupName.equals(BLANK_STRING)) {
+                            LOGGER.info("Policy Group Name is null or empty for partner id : " + partner.getId());
+                            throw new PartnerServiceException(ErrorCode.POLICY_GROUP_NOT_EXISTS.getErrorCode(),
+                                    ErrorCode.POLICY_GROUP_NOT_EXISTS.getErrorMessage());
+                        }
+                        List<PartnerPolicyRequest> partnerPolicyRequestList = partner.getPartnerPolicyRequests();
+                        if (!partnerPolicyRequestList.isEmpty()) {
+                            for (PartnerPolicyRequest partnerPolicyRequest : partnerPolicyRequestList) {
+                                AuthPolicy policyDetails = authPolicyRepository.findByPolicyGroupAndId(partner.getPolicyGroupId(), partnerPolicyRequest.getPolicyId());
+                                if (Objects.nonNull(policyDetails)) {
+                                    PolicyDto policyDto = new PolicyDto();
+                                    policyDto.setPartnerId(partner.getId());
+                                    policyDto.setPartnerType(partner.getPartnerTypeCode());
+                                    policyDto.setPolicyGroup(policyGroupName);
+                                    policyDto.setPolicyName(policyDetails.getName());
+                                    policyDto.setCreateDate(partnerPolicyRequest.getCrDtimes());
+                                    policyDto.setStatus(partnerPolicyRequest.getStatusCode());
+                                    policyDtoList.add(policyDto);
+                                }
+                            }
+                        }
+                    } catch (PartnerServiceException ex) {
+                        LOGGER.info("Could not fetch policies :" + ex.getMessage());
+                    }
+                }
+            } else {
+                LOGGER.info("sessionId", "idType", "id", "User id does not exists.");
+                throw new PartnerServiceException(ErrorCode.USER_ID_NOT_EXISTS.getErrorCode(),
+                        ErrorCode.USER_ID_NOT_EXISTS.getErrorMessage());
+            }
+        }catch (PartnerServiceException ex) {
+            LOGGER.info("sessionId", "idType", "id", "In getAllPolicies method of MultiPartnerServiceImpl - " + ex.getMessage());
+            throw ex;
+        } catch (Exception ex) {
+            LOGGER.debug("sessionId", "idType", "id", ex.getStackTrace());
+            LOGGER.error("sessionId", "idType", "id",
+                    "In getAllPolicies method of MultiPartnerServiceImpl - " + ex.getMessage());
+            throw new PartnerServiceException(ErrorCode.PARTNER_POLICY_FETCH_ERROR.getErrorCode(),
+                    ErrorCode.PARTNER_POLICY_FETCH_ERROR.getErrorMessage());
+        }
+        return policyDtoList;
     }
 
     private AuthUserDetails authUserDetails() {
