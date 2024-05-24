73a5314f6553c58d0594f1d300b0857d2e3338e1
kameshsr <kameshsr1338@gmail.com>
2024-05-24 10:21:39 +0530

    MOSIP-32842 Added git commit id plugin
    Signed-off-by: kameshsr <kameshsr1338@gmail.com>

--- partner/partner-management-service/src/main/java/io/mosip/pms/exception/PartnerServiceResponseExceptionHandler.java
+++ partner/partner-management-service/src/main/java/io/mosip/pms/exception/PartnerServiceResponseExceptionHandler.java
@@ -10,14 +10,12 @@ import java.util.Map;
 
 import jakarta.servlet.http.HttpServletRequest;
 
-import org.apache.hc.core5.http.Header;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
-import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
