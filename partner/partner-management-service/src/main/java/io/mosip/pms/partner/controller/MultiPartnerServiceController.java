64758ebab947ba183533e1d9b92a7888e37a868b
SwethaKrish4 <110164892+SwethaKrish4@users.noreply.github.com>
2024-05-31 15:33:18 +0530

    MOSIP-33487: Created new API getDetailsForAllPartnerTypes (#693)
    * MOSIP-33487: Created new API getDetailsForAllPartnerTypes
    
    Signed-off-by: SwethaKrish4 <110164892+SwethaKrish4@users.noreply.github.com>
    
    * MOSIP-33487: Created new API getDetailsForAllPartnerTypes
    
    Signed-off-by: SwethaKrish4 <110164892+SwethaKrish4@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: SwethaKrish4 <110164892+SwethaKrish4@users.noreply.github.com>

--- partner/partner-management-service/src/main/java/io/mosip/pms/partner/controller/MultiPartnerServiceController.java
+++ partner/partner-management-service/src/main/java/io/mosip/pms/partner/controller/MultiPartnerServiceController.java
@@ -2,6 +2,7 @@ package io.mosip.pms.partner.controller;
 
 import io.mosip.pms.common.response.dto.ResponseWrapper;
 import io.mosip.pms.partner.dto.CertificateDto;
+import io.mosip.pms.partner.dto.PartnerTypesDto;
 import io.mosip.pms.partner.dto.PolicyDto;
 import io.mosip.pms.partner.service.MultiPartnerService;
 import io.swagger.annotations.Api;
@@ -48,6 +49,19 @@ public class MultiPartnerServiceController {
     public ResponseWrapper<List<PolicyDto>> getAllPolicies() {
         ResponseWrapper<List<PolicyDto>> responseWrapper = new ResponseWrapper<>();
         responseWrapper.setResponse(multiPartnerService.getAllPolicies());
+        return responseWrapper;
+    }
+
+    @GetMapping(value = "/getDetailsForAllPartnerTypes")
+    @Operation(summary = "Get details for all partner types", description = "fetch details for all partner types")
+    @ApiResponses(value = {@ApiResponse(responseCode = "200", description = "OK"),
+            @ApiResponse(responseCode = "201", description = "Created", content = @Content(schema = @Schema(hidden = true))),
+            @ApiResponse(responseCode = "401", description = "Unauthorized", content = @Content(schema = @Schema(hidden = true))),
+            @ApiResponse(responseCode = "403", description = "Forbidden", content = @Content(schema = @Schema(hidden = true))),
+            @ApiResponse(responseCode = "404", description = "Not Found", content = @Content(schema = @Schema(hidden = true)))})
+    public ResponseWrapper<List<PartnerTypesDto>> getDetailsForAllPartnerTypes() {
+        ResponseWrapper<List<PartnerTypesDto>> responseWrapper = new ResponseWrapper<>();
+        responseWrapper.setResponse(multiPartnerService.getDetailsForAllPartnerTypes());
         return responseWrapper;
     }
 }
---
+++ partner/partner-management-service/src/main/java/io/mosip/pms/partner/dto/PartnerTypesDto.java
@@ -0,0 +1,15 @@
+package io.mosip.pms.partner.dto;
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+@Data
+@Getter
+@Setter
+public class PartnerTypesDto {
+    private String partnerId;
+    private String partnerType;
+    private String policyGroupId;
+    private String policyGroupName;
+}
