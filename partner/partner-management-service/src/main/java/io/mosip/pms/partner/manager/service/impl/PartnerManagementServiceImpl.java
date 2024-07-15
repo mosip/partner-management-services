14273f2869cce63b82269f71e248fee32f3f919f
Mayura Deshmukh <mayura.deshmukh@gmail.com>
2024-07-15 13:20:06 +0530

    MOSIP-34430 checking status before deactivating API key
    Signed-off-by: Mayura Deshmukh <mayura.deshmukh@gmail.com>

--- partner/partner-management-service/src/main/java/io/mosip/pms/partner/manager/service/impl/PartnerManagementServiceImpl.java
+++ partner/partner-management-service/src/main/java/io/mosip/pms/partner/manager/service/impl/PartnerManagementServiceImpl.java
@@ -675,6 +675,23 @@ public class PartnerManagementServiceImpl implements PartnerManagerService {
 			throw new PartnerManagerServiceException(ErrorCode.PARTNER_POLICY_LABEL_NOT_EXISTS.getErrorCode(),
 					ErrorCode.PARTNER_POLICY_LABEL_NOT_EXISTS.getErrorMessage());
 		}
+		// check if Partner is Active or not
+		if (policyByLabel.getPartner() != null && !policyByLabel.getPartner().getIsActive()) {
+			LOGGER.error("Partner is not Active, hence status of API key cannot be updated, for partner: " + partnerId);
+			auditUtil.setAuditRequestDto(PartnerManageEnum.ACTIVATE_DEACTIVATE_API_PARTNERS_FAILED, partnerId,
+					"partnerId");
+			throw new PartnerManagerServiceException(ErrorCode.PARTNER_NOT_ACTIVE_EXCEPTION.getErrorCode(),
+					ErrorCode.PARTNER_NOT_ACTIVE_EXCEPTION.getErrorMessage());
+		}
+		// check if API key has been already deactivated
+		if (!policyByLabel.getIsActive() && request.getStatus().equalsIgnoreCase(PartnerConstants.DEACTIVE)) {
+			LOGGER.error(
+					"API key is already deactivated hence it cannot be deactivated again, for partner: " + partnerId);
+			auditUtil.setAuditRequestDto(PartnerManageEnum.ACTIVATE_DEACTIVATE_API_PARTNERS_FAILED, partnerId,
+					"partnerId");
+			throw new PartnerManagerServiceException(ErrorCode.PARTNER_APIKEY_NOT_ACTIVE_EXCEPTION.getErrorCode(),
+					ErrorCode.PARTNER_APIKEY_NOT_ACTIVE_EXCEPTION.getErrorMessage());
+		}
 		policyByLabel.setUpdBy(getUser());
 		policyByLabel.setUpdDtimes(Timestamp.valueOf(LocalDateTime.now()));		
 		if (request.getStatus().equalsIgnoreCase(PartnerConstants.ACTIVE)) {
