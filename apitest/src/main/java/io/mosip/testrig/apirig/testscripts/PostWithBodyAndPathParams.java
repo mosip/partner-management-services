9fa06cc01dcff372b2f8495ba7e4d190cd32e32c
Nandhukumar <nandhukumare@gmail.com>
2024-09-18 11:54:08 +0530

    MOSIP-35770
    Signed-off-by: Nandhukumar <nandhukumare@gmail.com>

--- apitest/src/main/java/io/mosip/testrig/apirig/testscripts/PostWithBodyAndPathParams.java
+++ apitest/src/main/java/io/mosip/testrig/apirig/testscripts/PostWithBodyAndPathParams.java
@@ -30,6 +30,7 @@ import io.mosip.testrig.apirig.utils.AuthenticationTestException;
 import io.mosip.testrig.apirig.utils.ConfigManager;
 import io.mosip.testrig.apirig.utils.GlobalConstants;
 import io.mosip.testrig.apirig.utils.OutputValidationUtil;
+import io.mosip.testrig.apirig.utils.PMSUtil;
 import io.mosip.testrig.apirig.utils.ReportUtil;
 import io.restassured.response.Response;
 
@@ -83,7 +84,7 @@ public class PostWithBodyAndPathParams extends AdminTestUtil implements ITest {
 	public void test(TestCaseDTO testCaseDTO) throws AuthenticationTestException, AdminTestException {
 		testCaseName = testCaseDTO.getTestCaseName();
 		String[] templateFields = testCaseDTO.getTemplateFields();
-
+		testCaseName = PMSUtil.isTestCaseValidForExecution(testCaseDTO);
 		if (HealthChecker.signalTerminateExecution) {
 			throw new SkipException(
 					GlobalConstants.TARGET_ENV_HEALTH_CHECK_FAILED + HealthChecker.healthCheckFailureMapS);
