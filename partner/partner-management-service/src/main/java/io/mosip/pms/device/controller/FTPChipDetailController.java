67284320264c39b5c71b7c1b8967a83365d28856
Swetha K <swetha.k@technoforte.co.in>
2024-11-28 18:24:01 +0530

    MOSIP-37875: Created v2 endpoint for ftpChipDetails
    Signed-off-by: Swetha K <swetha.k@technoforte.co.in>

--- partner/partner-management-service/src/main/java/io/mosip/pms/device/controller/FTPChipDetailController.java
+++ partner/partner-management-service/src/main/java/io/mosip/pms/device/controller/FTPChipDetailController.java
@@ -24,7 +24,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.fasterxml.jackson.core.JsonParseException;
 import com.fasterxml.jackson.core.JsonProcessingException;
