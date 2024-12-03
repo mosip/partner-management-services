613431127ecb7f1c83c42ae376cb30c115c21568
Anup Nehe <anup.nehe@technoforte.co.in>
2024-12-03 10:14:59 +0530

    MOSIP-34116
    Signed-off-by: Anup Nehe <anup.nehe@technoforte.co.in>

--- api-test/src/main/java/io/mosip/testrig/apirig/utils/PMSRevampUtil.java
+++ api-test/src/main/java/io/mosip/testrig/apirig/utils/PMSRevampUtil.java
@@ -2,7 +2,9 @@ package io.mosip.testrig.apirig.utils;
 import org.apache.log4j.Logger;
 import org.testng.SkipException;
 
+import io.mosip.testrig.apirig.dbaccess.DBManager;
 import io.mosip.testrig.apirig.dto.TestCaseDTO;
+import io.mosip.testrig.apirig.testrunner.BaseTestCase;
 
 public class PMSRevampUtil extends AdminTestUtil {
 
@@ -16,4 +18,19 @@ public class PMSRevampUtil extends AdminTestUtil {
 		}
 		return testCaseName;
 }
+	
+	public static void DbCleanRevamp() {
+		BaseTestCase.currentModule = GlobalConstants.PARTNERNEW;
+		DBManager.executeDBQueries(PMSRevampConfigManger.getPMSDbUrl(), PMSRevampConfigManger.getPMSDbUser(),
+				PMSRevampConfigManger.getPMSDbPass(), PMSRevampConfigManger.getPMSDbSchema(),
+				getGlobalResourcePath() + "/" + "config/partnerRevampDataDeleteQueries.txt");
+
+		DBManager.executeDBQueries(PMSRevampConfigManger.getKMDbUrl(), PMSRevampConfigManger.getKMDbUser(),
+				PMSRevampConfigManger.getKMDbPass(), PMSRevampConfigManger.getKMDbSchema(),
+				getGlobalResourcePath() + "/" + "config/partnerRevampDataDeleteQueriesForKeyMgr.txt");
+
+		DBManager.executeDBQueries(PMSRevampConfigManger.getIdaDbUrl(), PMSRevampConfigManger.getIdaDbUser(),
+				PMSRevampConfigManger.getPMSDbPass(), PMSRevampConfigManger.getIdaDbSchema(),
+				getGlobalResourcePath() + "/" + "config/partnerRevampDataDeleteQueriesForIDA.txt");
+	}
 }
