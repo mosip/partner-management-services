844125f0df27b9af4612ebe9e612b7e0f79b1e69
Mohanachandran S <mohanachandran.s@technoforte.co.in>
2024-12-23 13:17:10 +0530

    MOSIP-38543 - synced release branch with develop branch
    Signed-off-by: Mohanachandran S <mohanachandran.s@technoforte.co.in>

--- apitest/entrypoint.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-## Run automationtests
-java -jar -Dmodules="$MODULES" -Denv.user="$ENV_USER" -Denv.endpoint="$ENV_ENDPOINT" -Denv.testLevel="$ENV_TESTLEVEL" apitest-pms-*-jar-with-dependencies.jar;
