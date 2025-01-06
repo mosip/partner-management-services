dbb29a37b61bc36edb55cf377d03b40b1571cded
Sudeep7353 <103890430+Sudeep7353@users.noreply.github.com>
2025-01-06 12:36:28 +0530

    MOSIP-36037 : Sonar coverage (#1064)
    Signed-off-by: sudeep <sudeep.j7353@gmail.com>

--- partner/partner-management-service/src/test/java/io/mosip/pms/test/oauth/controller/ClientManagementControllerTest.java
+++ partner/partner-management-service/src/test/java/io/mosip/pms/test/oauth/controller/ClientManagementControllerTest.java
@@ -58,45 +58,45 @@ public class ClientManagementControllerTest {
         public_key.put(  "kid", "1bbdc9de-c24f-4801-b6b3-691ac07641af");
         public_key.put( "alg", "RS256");
         public_key.put(  "n","wXGQA574CU-WTWPILd4S3_1sJf0Yof0kwMeNctXc1thQo70Ljfn9f4igpRe7f8qNs_W6dLuLWemFhGJBQBQ7vvickECKNJfo_EzSD_yyPCg7k_AGbTWTkuoObHrpilwJGyKVSkOIujH_FqHIVkwkVXjWc25Lsb8Gq4nAHNQEqqgaYPLEi5evCR6S0FzcXTPuRh9zH-cM0Onjv4orrfYpEr61HcRp5MXL55b7yBoIYlXD8NfalcgdrWzp4VZHvQ8yT9G5eaf27XUn6ZBeBf7VnELcKFTyw1pK2wqoOxRBc8Y1wO6rEy8PlCU6wD-mbIzcjG1wUfnbgvJOM4A5G41quQ");
+
     }
 
+
     @Test
+    @WithMockUser(roles = {"AUTH_PARTNER"})
     public void testCreateOIDCClient() throws Exception {
-        io.mosip.pms.common.request.dto.RequestWrapper<ClientDetailCreateRequest> requestWrapper = new io.mosip.pms.common.request.dto.RequestWrapper<>();
-        requestWrapper.setId("42");
-        requestWrapper.setMetadata("Metadata");
-        requestWrapper.setRequest(new ClientDetailCreateRequest());
-        requestWrapper.setRequesttime(null);
-        requestWrapper.setVersion("1.0.2");
-        String content = (new ObjectMapper()).writeValueAsString(requestWrapper);
-        MockHttpServletRequestBuilder requestBuilder = MockMvcRequestBuilders.post("/oauth/client")
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(content);
-        ResultActions actualPerformResult = MockMvcBuilders.standaloneSetup(clientManagementController)
-                .build()
-                .perform(requestBuilder);
-        actualPerformResult.andExpect(MockMvcResultMatchers.status().is(400));
+        RequestWrapper<ClientDetailCreateRequestV2> requestWrapper = new RequestWrapper<>();
+        ClientDetailCreateRequestV2 clientDetailCreateRequestV2 = new ClientDetailCreateRequestV2();
+        Map<String, String> clientNameLangMap = new HashMap<>();
+        clientNameLangMap.put("eng", "English Client Name");
+        clientNameLangMap.put("fra", "French Client Name");
+        clientNameLangMap.put("ara", "Arabic Client Name");
+
+        clientDetailCreateRequestV2.setClientNameLangMap(clientNameLangMap);
+        clientDetailCreateRequestV2.setName("Mock Name");
+        clientDetailCreateRequestV2.setPolicyId("policy-id");
+        clientDetailCreateRequestV2.setPublicKey(public_key);
+        clientDetailCreateRequestV2.setAuthPartnerId("auth-partner-id");
+        clientDetailCreateRequestV2.setLogoUri("https://example.com/logo.png");
+        List<String> redirectUris = List.of("https://example.com/redirect1");
+        clientDetailCreateRequestV2.setRedirectUris(redirectUris);
+        List<String> grantTypes = Arrays.asList("authorization_code", "refresh_token");
+        clientDetailCreateRequestV2.setGrantTypes(grantTypes);
+        List<String> clientAuthMethods = List.of("private_key_jwt");
+        clientDetailCreateRequestV2.setClientAuthMethods(clientAuthMethods);
+
+        requestWrapper.setRequest(clientDetailCreateRequestV2);
+        String requestJson = new ObjectMapper().writeValueAsString(requestWrapper);
+
+        mockMvc.perform(MockMvcRequestBuilders.post("/oauth/client")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(requestJson))
+                .andExpect(MockMvcResultMatchers.status().isOk());
+
     }
 
     @Test
-    public void testUpdateClient() throws Exception {
-        io.mosip.pms.common.request.dto.RequestWrapper<ClientDetailUpdateRequest> requestWrapper = new io.mosip.pms.common.request.dto.RequestWrapper<>();
-        requestWrapper.setId("42");
-        requestWrapper.setMetadata("Metadata");
-        requestWrapper.setRequest(new ClientDetailUpdateRequest());
-        requestWrapper.setRequesttime(null);
-        requestWrapper.setVersion("1.0.2");
-        String content = (new ObjectMapper()).writeValueAsString(requestWrapper);
-        MockHttpServletRequestBuilder requestBuilder = MockMvcRequestBuilders.put("/oauth/client/{client_id}", "Client id")
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(content);
-        ResultActions actualPerformResult = MockMvcBuilders.standaloneSetup(clientManagementController)
-                .build()
-                .perform(requestBuilder);
-        actualPerformResult.andExpect(MockMvcResultMatchers.status().is(400));
-    }
-
-    @Test
+    @WithMockUser(roles = {"AUTH_PARTNER"})
     public void testCreateOAUTHClient() throws Exception {
         io.mosip.pms.common.request.dto.RequestWrapper<ClientDetailCreateRequestV2> requestWrapper = new RequestWrapper<>();
         requestWrapper.setId("42");
@@ -115,25 +115,36 @@ public class ClientManagementControllerTest {
     }
 
     @Test
+    @WithMockUser(roles = {"AUTH_PARTNER"})
     public void testUpdateOAUTHClient() throws Exception {
-        io.mosip.pms.common.request.dto.RequestWrapper<ClientDetailUpdateRequestV2> requestWrapper = new io.mosip.pms.common.request.dto.RequestWrapper<>();
-        requestWrapper.setId("42");
-        requestWrapper.setMetadata("Metadata");
-        requestWrapper.setRequest(new ClientDetailUpdateRequestV2());
-        requestWrapper.setRequesttime(null);
-        requestWrapper.setVersion("1.0.2");
-        String content = (new ObjectMapper()).writeValueAsString(requestWrapper);
-        MockHttpServletRequestBuilder requestBuilder = MockMvcRequestBuilders
-                .put("/oauth/client/{client_id}", "Client id")
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(content);
-        ResultActions actualPerformResult = MockMvcBuilders.standaloneSetup(clientManagementController)
-                .build()
-                .perform(requestBuilder);
-        actualPerformResult.andExpect(MockMvcResultMatchers.status().is(400));
+        RequestWrapper<ClientDetailUpdateRequestV2> requestWrapper = new RequestWrapper<>();
+        ClientDetailUpdateRequestV2 clientDetailUpdateRequestV2 = new ClientDetailUpdateRequestV2();
+        Map<String, String> clientNameLangMap = new HashMap<>();
+        clientNameLangMap.put("eng", "English Client Name");
+        clientNameLangMap.put("fra", "French Client Name");
+        clientNameLangMap.put("ara", "Arabic Client Name");
+
+        clientDetailUpdateRequestV2.setClientNameLangMap(clientNameLangMap);
+        clientDetailUpdateRequestV2.setClientName("Mock Name");
+        clientDetailUpdateRequestV2.setLogoUri("https://example.com/logo.png");
+        clientDetailUpdateRequestV2.setStatus("ACTIVE");
+        List<String> redirectUris = List.of("https://example.com/redirect1");
+        clientDetailUpdateRequestV2.setRedirectUris(redirectUris);
+        List<String> grantTypes = Arrays.asList("authorization_code", "refresh_token");
+        clientDetailUpdateRequestV2.setGrantTypes(grantTypes);
+        List<String> clientAuthMethods = List.of("private_key_jwt");
+        clientDetailUpdateRequestV2.setClientAuthMethods(clientAuthMethods);
+        requestWrapper.setRequest(clientDetailUpdateRequestV2);
+        String requestJson = new ObjectMapper().writeValueAsString(requestWrapper);
+
+        mockMvc.perform(MockMvcRequestBuilders.put("/oauth/client/123")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(requestJson))
+                .andExpect(MockMvcResultMatchers.status().isOk());
     }
 
     @Test ()
+    @WithMockUser(roles = {"AUTH_PARTNER"})
     public void testGetOAuthClient() throws Exception {
         io.mosip.pms.oauth.client.dto.ClientDetail clientDetail = new io.mosip.pms.oauth.client.dto.ClientDetail();
         clientDetail.setAcrValues(Collections.singletonList("Value"));
