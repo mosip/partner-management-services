a1e2fda35ce1a572a9b1214d4dd937e21d4e19db
sudeep <sudeep.j7353@gmail.com>
2025-01-09 11:52:09 +0530

    MOSIP-32075 : DB attributes of PMS are hardcoded in db_scripts (sql scripts)
    Signed-off-by: sudeep <sudeep.j7353@gmail.com>

--- db_upgrade_scripts/mosip_pms/upgrade.sh
+++ db_upgrade_scripts/mosip_pms/upgrade.sh
@@ -7,16 +7,21 @@ SOURCE_DB1_SUPPORT_FILE=sql/1.1.5.5_to_1.2.0.1-B1_pms-authdevice-support.sql
 SOURCE_DB2_SUPPORT_FILE=sql/1.1.5.5_to_1.2.0.1-B1_pms-regdevice-support.sql
 properties_file="$1"
 echo `date "+%m/%d/%Y %H:%M:%S"` ": $properties_file"
-if [ -f "$properties_file" ]
-then
-     echo `date "+%m/%d/%Y %H:%M:%S"` ": Property file \"$properties_file\" found."
-    while IFS='=' read -r key value
-    do
-        key=$(echo $key | tr '.' '_')
-         eval ${key}=\${value}
+if [ -f "$properties_file" ]; then
+    echo "$(date "+%m/%d/%Y %H:%M:%S") : Property file \"$properties_file\" found."
+    while IFS='=' read -r key value || [ -n "$key" ]; do
+        # Skip empty lines or lines starting with #
+        [[ -z "$key" || "$key" =~ ^# ]] && continue
+
+        # Trim spaces and replace dots in keys with underscores
+        key=$(echo "$key" | tr -d ' ' | tr '.' '_')
+        value=$(echo "$value" | xargs) # Trim spaces around the value
+        
+        # Set the variable dynamically
+        eval "${key}=\"${value}\""
     done < "$properties_file"
 else
-     echo `date "+%m/%d/%Y %H:%M:%S"` ": Property file not found, Pass property file name as argument."
+    echo "$(date "+%m/%d/%Y %H:%M:%S") : Property file not found. Pass property file name as argument."
 fi
 
 echo "Current version: $CURRENT_VERSION"
