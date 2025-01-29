b28413bb76da4ecdcababc808096c656a775ad21
sudeep <sudeep.j7353@gmail.com>
2025-01-29 15:14:25 +0530

    MOSIP-35546 : updated upgrade scripts.
    Signed-off-by: sudeep <sudeep.j7353@gmail.com>

--- db_upgrade_scripts/mosip_pms/sql/1.3.0-dp.1_to_1.3.0_upgrade.sql
+++ db_upgrade_scripts/mosip_pms/sql/1.3.0-dp.1_to_1.3.0_upgrade.sql
@@ -1,6 +1,6 @@
 -- -------------------------------------------------------------------------------------------------
 -- Database Name    : mosip_pms
--- Release Version 	: 1.4.0
+-- Release Version 	: 1.3.0-beta.1
 -- Purpose    		: Database Alter scripts for the release for PMS DB.       
 -- Create By   		: Swetha K
 -- Created Date		: Aug-2024
@@ -8,18 +8,68 @@
 -- Modified Date        Modified By         Comments / Remarks
 -- -------------------------------------------------------------------------------------------------
 \c mosip_pms
--- Dropping unique constraint from pms.device_detail table
-ALTER TABLE pms.device_detail DROP CONSTRAINT uk_devdtl_id;
 
--- Creating unique index for make, model, and approval status
-CREATE UNIQUE INDEX uk_devdtl_make_model_approval_status
-ON pms.device_detail (dprovider_id,dtype_code,dstype_code,make,model)
-WHERE approval_status != 'rejected' AND NOT (approval_status = 'approved' AND is_active = false);
+-- Dropping unique constraint from pms.device_detail table if it exists
+DO $$
+BEGIN
+    IF EXISTS (
+        SELECT 1 
+        FROM pg_constraint 
+        WHERE conname = 'uk_devdtl_id' 
+        AND conrelid = 'pms.device_detail'::regclass
+    ) THEN
+        ALTER TABLE pms.device_detail DROP CONSTRAINT uk_devdtl_id;
+    END IF;
+END $$;
 
--- Dropping unique constraint from pms.ftp_chip_detail table
-ALTER TABLE pms.ftp_chip_detail DROP CONSTRAINT uk_fcdtl_id;
+-- Creating unique index for make, model, and approval status if it does not exist
+DO $$
+BEGIN
+    IF NOT EXISTS (
+        SELECT 1 
+        FROM pg_indexes 
+        WHERE indexname = 'uk_devdtl_make_model_approval_status' 
+        AND schemaname = 'pms'
+    ) THEN
+        CREATE UNIQUE INDEX uk_devdtl_make_model_approval_status
+        ON pms.device_detail (dprovider_id, dtype_code, dstype_code, make, model)
+        WHERE approval_status != 'rejected' AND NOT (approval_status = 'approved' AND is_active = false);
+    END IF;
+END $$;
 
--- Creating unique index for make, model, and approval status
-CREATE UNIQUE INDEX uk_fcdtl_make_model_approval_status
-ON pms.ftp_chip_detail (foundational_trust_provider_id, make, model)
-WHERE approval_status != 'rejected' AND NOT (approval_status = 'approved' AND is_active = false);
+-- Dropping unique constraint from pms.ftp_chip_detail table if it exists
+DO $$
+BEGIN
+    IF EXISTS (
+        SELECT 1 
+        FROM pg_constraint 
+        WHERE conname = 'uk_fcdtl_id' 
+        AND conrelid = 'pms.ftp_chip_detail'::regclass
+    ) THEN
+        ALTER TABLE pms.ftp_chip_detail DROP CONSTRAINT uk_fcdtl_id;
+    END IF;
+END $$;
+
+-- Creating unique index for make, model, and approval status if it does not exist
+DO $$
+BEGIN
+    IF NOT EXISTS (
+        SELECT 1 
+        FROM pg_indexes 
+        WHERE indexname = 'uk_fcdtl_make_model_approval_status' 
+        AND schemaname = 'pms'
+    ) THEN
+        CREATE UNIQUE INDEX uk_fcdtl_make_model_approval_status
+        ON pms.ftp_chip_detail (foundational_trust_provider_id, make, model)
+        WHERE approval_status != 'rejected' AND NOT (approval_status = 'approved' AND is_active = false);
+    END IF;
+END $$;
+
+-- Updating policy_file_id in pms.auth_policy for specific IDs
+UPDATE pms.auth_policy
+SET policy_file_id='{"dataSharePolicies":{"typeOfShare":"Data Share","validForInMinutes":"30","transactionsAllowed":"2","encryptionType":"Partner Based","shareDomain":"datashare.datashare","source":"ID Repository"},"shareableAttributes":[{"attributeName":"fullName","source":[{"attribute":"fullName","filter":[{"language":"eng"}]}],"encrypted":false},{"attributeName":"dateOfBirth","source":[{"attribute":"dateOfBirth"}],"encrypted":false,"format":"YYYY"},{"attributeName":"gender","source":[{"attribute":"gender","filter":[{"language":"eng"}]}],"encrypted":false},{"attributeName":"phone","source":[{"attribute":"phone"}],"encrypted":false},{"attributeName":"email","source":[{"attribute":"email"}],"encrypted":false},{"attributeName":"addressLine1","source":[{"attribute":"addressLine1","filter":[{"language":"eng"}]}],"encrypted":false},{"attributeName":"addressLine2","source":[{"attribute":"addressLine2","filter":[{"language":"eng"}]}],"encrypted":false},{"attributeName":"addressLine3","source":[{"attribute":"addressLine3","filter":[{"language":"eng"}]}],"encrypted":false},{"attributeName":"region","source":[{"attribute":"region","filter":[{"language":"eng"}]}],"encrypted":false},{"attributeName":"province","source":[{"attribute":"province","filter":[{"language":"eng"}]}],"encrypted":false},{"attributeName":"city","source":[{"attribute":"city","filter":[{"language":"eng"}]}],"encrypted":false},{"attributeName":"UIN","source":[{"attribute":"UIN"}],"encrypted":false},{"attributeName":"postalCode","source":[{"attribute":"postalCode"}],"encrypted":false},{"attributeName":"biometrics","group":"CBEFF","source":[{"attribute":"individualBiometrics","filter":[{"type":"Face"},{"type":"Finger","subType":["Left Thumb","Right Thumb"]}]}],"encrypted":true,"format":"extraction"}]}'
+WHERE id='mpolicy-default-qrcode';
+
+UPDATE pms.auth_policy
+SET policy_file_id='{"dataSharePolicies":{"typeOfShare":"Data Share","validForInMinutes":"30","transactionsAllowed":"2","encryptionType":"Partner Based","shareDomain":"datashare.datashare","source":"ID Repository"},"shareableAttributes":[{"attributeName":"fullName","source":[{"attribute":"fullName","filter":[{"language":"eng"}]}],"encrypted":false},{"attributeName":"dateOfBirth","source":[{"attribute":"dateOfBirth"}],"encrypted":false,"format":"YYYY"},{"attributeName":"gender","source":[{"attribute":"gender","filter":[{"language":"eng"}]}],"encrypted":false},{"attributeName":"phone","source":[{"attribute":"phone"}],"encrypted":false},{"attributeName":"email","source":[{"attribute":"email"}],"encrypted":false},{"attributeName":"addressLine1","source":[{"attribute":"addressLine1","filter":[{"language":"eng"}]}],"encrypted":false},{"attributeName":"addressLine2","source":[{"attribute":"addressLine2","filter":[{"language":"eng"}]}],"encrypted":false},{"attributeName":"addressLine3","source":[{"attribute":"addressLine3","filter":[{"language":"eng"}]}],"encrypted":false},{"attributeName":"region","source":[{"attribute":"region","filter":[{"language":"eng"}]}],"encrypted":false},{"attributeName":"province","source":[{"attribute":"province","filter":[{"language":"eng"}]}],"encrypted":false},{"attributeName":"city","source":[{"attribute":"city","filter":[{"language":"eng"}]}],"encrypted":false},{"attributeName":"UIN","source":[{"attribute":"UIN"}],"encrypted":false},{"attributeName":"postalCode","source":[{"attribute":"postalCode"}],"encrypted":false},{"attributeName":"biometrics","group":"CBEFF","source":[{"attribute":"individualBiometrics","filter":[{"type":"Face"},{"type":"Finger","subType":["Left Thumb","Right Thumb"]}]}],"encrypted":true,"format":"extraction"}]}'
+WHERE id='mpolicy-default-reprint';
