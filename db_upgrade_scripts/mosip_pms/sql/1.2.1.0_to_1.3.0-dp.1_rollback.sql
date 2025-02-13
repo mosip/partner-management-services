07235860e263c9ec011c477cf9c283f5399169ea
sudeep <sudeep.j7353@gmail.com>
2025-02-13 13:51:03 +0530

    MOSIP-39372 : updated develop branch upgrade scripts
    Signed-off-by: sudeep <sudeep.j7353@gmail.com>

--- db_upgrade_scripts/mosip_pms/sql/1.2.1.0_to_1.3.0-dp.1_rollback.sql
+++ db_upgrade_scripts/mosip_pms/sql/1.2.1.0_to_1.3.0-dp.1_rollback.sql
@@ -1,54 +1,7 @@
--- -------------------------------------------------------------------------------------------------
--- Database Name    : mosip_pms
--- Release Version 	: release-1.2.2.0
--- Purpose    		: Database Alter scripts for the release for PMS DB.
--- Create By   		: Swetha K
--- Created Date		: Aug-2024
---
--- Modified Date        Modified By         Comments / Remarks
--- -------------------------------------------------------------------------------------------------
 \c mosip_pms
 
 -- user_details
 DROP TABLE IF EXISTS pms.user_details;
-
--- Dropping unique index from pms.device_detail table if it exists
-DROP INDEX IF EXISTS pms.uk_devdtl_make_model_approval_status;
-
--- Adding unique constraint for dprovider_id, dtype_code, dstype_code, make, and model
--- Only if the constraint does not already exist
-DO $$
-BEGIN
-    IF NOT EXISTS (
-        SELECT 1
-        FROM pg_constraint
-        WHERE conname = 'uk_devdtl_id'
-        AND conrelid = 'pms.device_detail'::regclass
-    ) THEN
-        ALTER TABLE pms.device_detail
-        ADD CONSTRAINT uk_devdtl_id
-        UNIQUE (dprovider_id, dtype_code, dstype_code, make, model);
-    END IF;
-END $$;
-
--- Dropping unique index from pms.ftp_chip_detail table if it exists
-DROP INDEX IF EXISTS pms.uk_fcdtl_make_model_approval_status;
-
--- Adding unique constraint for foundational_trust_provider_id, make, and model
--- Only if the constraint does not already exist
-DO $$
-BEGIN
-    IF NOT EXISTS (
-        SELECT 1
-        FROM pg_constraint
-        WHERE conname = 'uk_fcdtl_id'
-        AND conrelid = 'pms.ftp_chip_detail'::regclass
-    ) THEN
-        ALTER TABLE pms.ftp_chip_detail
-        ADD CONSTRAINT uk_fcdtl_id
-        UNIQUE (foundational_trust_provider_id, make, model);
-    END IF;
-END $$;
 
 -- Updating policy_file_id in pms.auth_policy for specific IDs
 UPDATE pms.auth_policy
