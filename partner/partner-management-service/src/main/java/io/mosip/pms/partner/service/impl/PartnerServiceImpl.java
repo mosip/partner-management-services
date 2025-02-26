feaca5c4853c12a9718b2d0de89137d9d9a9192d
sudeep <sudeep.j7353@gmail.com>
2025-02-26 18:56:10 +0530

    removed validate user check in /{partnerId}/certificate endpoint
    Signed-off-by: sudeep <sudeep.j7353@gmail.com>

--- partner/partner-management-service/src/main/java/io/mosip/pms/partner/service/impl/PartnerServiceImpl.java
+++ partner/partner-management-service/src/main/java/io/mosip/pms/partner/service/impl/PartnerServiceImpl.java
@@ -830,7 +830,7 @@ public class PartnerServiceImpl implements PartnerService {
 
 	@Override
 	public PartnerCertDownloadResponeDto getPartnerCertificate(PartnerCertDownloadRequestDto certDownloadRequestDto) throws JsonProcessingException {
-		validateUser(certDownloadRequestDto);
+		// validateUser(certDownloadRequestDto);
 		// Fetch partner from DB
 		Optional<Partner> partnerFromDb = getPartner(certDownloadRequestDto);
 		Partner partner = partnerFromDb.get();
