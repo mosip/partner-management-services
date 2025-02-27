da735e3b21e9c6d942a7a5287e53059ead81f168
sudeep <sudeep.j7353@gmail.com>
2025-02-27 20:05:50 +0530

    MOSIP-39856 : Sprint Boot Batch Job Framework
    Signed-off-by: sudeep <sudeep.j7353@gmail.com>

--- partner/partner-management-batch-job/src/main/java/io/mosip/pms/partner/management/batch/job/config/SchedulerConfig.java
+++ partner/partner-management-batch-job/src/main/java/io/mosip/pms/partner/management/batch/job/config/SchedulerConfig.java
@@ -4,7 +4,6 @@ import org.springframework.batch.core.Job;
 import org.springframework.batch.core.JobParameters;
 import org.springframework.batch.core.JobParametersBuilder;
 import org.springframework.batch.core.launch.JobLauncher;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
