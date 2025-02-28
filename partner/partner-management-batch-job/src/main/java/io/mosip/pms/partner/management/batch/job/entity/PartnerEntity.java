c0f9660f219e84256cd148ed7a0db79bc085d67f
sudeep <sudeep.j7353@gmail.com>
2025-02-28 11:03:57 +0530

    MOSIP-39856 : Sprint Boot Batch Job Framework
    Signed-off-by: sudeep <sudeep.j7353@gmail.com>

---
+++ partner/partner-management-batch-job/src/main/java/io/mosip/pms/partner/management/batch/job/entity/PartnerEntity.java
@@ -0,0 +1,75 @@
+package io.mosip.pms.partner.management.batch.job.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+import java.sql.Timestamp;
+
+@Entity
+@Table(name="partner")
+@Data
+public class PartnerEntity {
+
+    @Id
+    private String id;
+
+    @Column(name="address")
+    private String address;
+
+    @Column(name="contact_no")
+    private String contactNo;
+
+    @Column(name="cr_by")
+    private String createdBy;
+
+    @Column(name="cr_dtimes")
+    private Timestamp createdDatetime;
+
+    @Column(name="del_dtimes")
+    private Timestamp deletedDatetime;
+
+    @Column(name="email_id")
+    private String emailId;
+
+    @Column(name="is_active")
+    private Boolean isActive;
+
+    @Column(name="is_deleted")
+    private Boolean isDeleted;
+
+    @Column(name="name")
+    private String name;
+
+    @Column(name="policy_group_id")
+    private String policyGroupId;
+
+    @Column(name="certificate_alias")
+    private String certificateAlias;
+
+    @Column(name = "partner_type_code")
+    private String partnerTypeCode;
+
+    @Column(name="approval_status")
+    private String approvalStatus;
+
+    @Column(name="upd_by")
+    private String updatedBy;
+
+    @Column(name="upd_dtimes")
+    private Timestamp updatedDatetime;
+
+    @Column(name="user_id")
+    private String userId;
+
+    @Column(name = "lang_code")
+    private String langCode;
+
+    @Column(name = "logo_url")
+    private String logoUrl;
+
+    @Column(name = "addl_info")
+    private String additionalInfo;
+}
