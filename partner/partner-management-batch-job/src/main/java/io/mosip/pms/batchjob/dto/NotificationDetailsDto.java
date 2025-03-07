0a8192deadfb1f2e068deb8450f023465984423f
sudeep <sudeep.j7353@gmail.com>
2025-03-07 12:25:50 +0530

    MOSIP-39322 : Email Notification
    Signed-off-by: sudeep <sudeep.j7353@gmail.com>

--- partner/partner-management-batch-job/src/main/java/io/mosip/pms/batchjob/dto/NotificationDetailsDto.java
+++ partner/partner-management-batch-job/src/main/java/io/mosip/pms/batchjob/dto/NotificationDetailsDto.java
@@ -7,4 +7,6 @@ import java.util.List;
 @Data
 public class NotificationDetailsDto {
     private List<CertificateDetailsDto> certificateDetails;
+    private List<SbiDetailsDto> sbiDetails;
+    private List<ApiKeyDetailsDto> apiKeyDetails;
 }
