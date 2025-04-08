b929de02b5b82b7b28f4c38194e36e90771cf3c8
Mayura Deshmukh <mayura.deshmukh@gmail.com>
2025-04-08 15:29:33 +0530

    fixing junits
    Signed-off-by: Mayura Deshmukh <mayura.deshmukh@gmail.com>

--- partner/partner-management-service/src/test/java/io/mosip/pms/test/PartnerManagementServiceTest.java
+++ partner/partner-management-service/src/test/java/io/mosip/pms/test/PartnerManagementServiceTest.java
@@ -2,15 +2,16 @@ package io.mosip.pms.test;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Import;
 
 import io.mosip.pms.test.config.TestSecurityConfig;
 
-
-@SpringBootApplication(scanBasePackages = {"io.mosip.pms"})
+@SpringBootApplication
+@ComponentScan(basePackages = { "io.mosip.pms.*"})
 @Import(TestSecurityConfig.class)
 public class PartnerManagementServiceTest {
-	
+
 	public static void main(String[] args) {
 		SpringApplication.run(PartnerManagementServiceTest.class, args);
 	}
