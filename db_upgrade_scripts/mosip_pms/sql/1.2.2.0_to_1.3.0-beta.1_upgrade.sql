626f0c4cb309219f1460aaa6fdd12e858821ab09
sudeep <sudeep.j7353@gmail.com>
2025-04-15 17:20:23 +0530

    MOSIP-38061 :PMS Legacy - DB has PII issues.
    Signed-off-by: sudeep <sudeep.j7353@gmail.com>

--- db_upgrade_scripts/mosip_pms/sql/1.2.2.0_to_1.3.0-beta.1_upgrade.sql
+++ db_upgrade_scripts/mosip_pms/sql/1.2.2.0_to_1.3.0-beta.1_upgrade.sql
@@ -129,3 +129,18 @@ WHERE id='mpolicy-default-qrcode';
 UPDATE pms.auth_policy
 SET  policy_file_id='{"dataSharePolicies":{"typeOfShare":"Data Share","validForInMinutes":"30","transactionsAllowed":"2","encryptionType":"Partner Based","shareDomain":"datashare.datashare","source":"ID Repository"},"shareableAttributes":[{"attributeName":"fullName","source":[{"attribute":"fullName","filter":[{"language":"eng"}]}],"encrypted":false},{"attributeName":"dateOfBirth","source":[{"attribute":"dateOfBirth"}],"encrypted":false,"format":"YYYY"},{"attributeName":"gender","source":[{"attribute":"gender","filter":[{"language":"eng"}]}],"encrypted":false},{"attributeName":"phone","source":[{"attribute":"phone"}],"encrypted":false},{"attributeName":"email","source":[{"attribute":"email"}],"encrypted":false},{"attributeName":"addressLine1","source":[{"attribute":"addressLine1","filter":[{"language":"eng"}]}],"encrypted":false},{"attributeName":"addressLine2","source":[{"attribute":"addressLine2","filter":[{"language":"eng"}]}],"encrypted":false},{"attributeName":"addressLine3","source":[{"attribute":"addressLine3","filter":[{"language":"eng"}]}],"encrypted":false},{"attributeName":"region","source":[{"attribute":"region","filter":[{"language":"eng"}]}],"encrypted":false},{"attributeName":"province","source":[{"attribute":"province","filter":[{"language":"eng"}]}],"encrypted":false},{"attributeName":"city","source":[{"attribute":"city","filter":[{"language":"eng"}]}],"encrypted":false},{"attributeName":"UIN","source":[{"attribute":"UIN"}],"encrypted":false},{"attributeName":"postalCode","source":[{"attribute":"postalCode"}],"encrypted":false},{"attributeName":"biometrics","group":"CBEFF","source":[{"attribute":"individualBiometrics","filter":[{"type":"Face"},{"type":"Finger","subType":["Left Thumb","Right Thumb"]}]}],"encrypted":true,"format":"extraction"}]}'
 WHERE id='mpolicy-default-euin';
+
+-- Update column sizes in pms.partner
+ALTER TABLE pms.partner
+    ALTER COLUMN contact_no TYPE character varying(500),
+    ALTER COLUMN email_id TYPE character varying(2000);
+
+-- Update column sizes in pms.partner_contact
+ALTER TABLE pms.partner_contact
+    ALTER COLUMN contact_no TYPE character varying(500),
+    ALTER COLUMN email_id TYPE character varying(2000);
+
+-- Update column sizes in pms.partner_h
+ALTER TABLE pms.partner_h
+    ALTER COLUMN contact_no TYPE character varying(500),
+    ALTER COLUMN email_id TYPE character varying(2000);
