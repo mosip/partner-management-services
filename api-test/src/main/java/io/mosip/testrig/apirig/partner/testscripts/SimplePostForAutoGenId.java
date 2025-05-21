461f7c5dfb7ecf3b2510782677062686856f6921
Sradha Mohanty <134414554+SradhaMohanty5899@users.noreply.github.com>
2025-05-21 11:57:00 +0530

    MOSIP-40258: Standardized XSS Exception Handling in apitest-pms Module
    Signed-off-by: SradhaMohanty5899 <mohantysradha10@gmail.com>

--- api-test/src/main/java/io/mosip/testrig/apirig/partner/testscripts/SimplePostForAutoGenId.java
+++ api-test/src/main/java/io/mosip/testrig/apirig/partner/testscripts/SimplePostForAutoGenId.java
@@ -32,6 +32,7 @@ import io.mosip.testrig.apirig.utils.AuthenticationTestException;
 import io.mosip.testrig.apirig.utils.GlobalConstants;
 import io.mosip.testrig.apirig.utils.OutputValidationUtil;
 import io.mosip.testrig.apirig.utils.ReportUtil;
+import io.mosip.testrig.apirig.utils.SecurityXSSException;
 import io.restassured.response.Response;
 
 public class SimplePostForAutoGenId extends PMSUtil implements ITest {
@@ -82,7 +83,7 @@ public class SimplePostForAutoGenId extends PMSUtil implements ITest {
 	 */
 	@Test(dataProvider = "testcaselist")
 	public void test(TestCaseDTO testCaseDTO)
-			throws AuthenticationTestException, AdminTestException, NoSuchAlgorithmException {
+			throws AuthenticationTestException, AdminTestException, NoSuchAlgorithmException, SecurityXSSException {
 		testCaseName = testCaseDTO.getTestCaseName();
 		testCaseName = PMSUtil.isTestCaseValidForExecution(testCaseDTO);
 		if (HealthChecker.signalTerminateExecution) {
