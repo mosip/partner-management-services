d99c32832fcd7d4f2944bcb539c6fbfd17581cad
Sudeep7353 <103890430+Sudeep7353@users.noreply.github.com>
2025-05-30 18:55:27 +0530

    added 1.2.0.1-B1_to_1.2.0.1-B2_rollback script (#1365)
    Signed-off-by: sudeep <sudeep.j7353@gmail.com>

--- db_upgrade_scripts/mosip_pms/sql/1.2.0.1-B1_to_1.2.0.1-B2_rollback.sql
+++ db_upgrade_scripts/mosip_pms/sql/1.2.0.1-B1_to_1.2.0.1-B2_rollback.sql
@@ -1,1 +1,13 @@
-\echo 'Upgrade Queries not required for transition from $CURRENT_VERSION to $UPGRADE_VERSION'
+\c mosip_pms
+
+-- Drop the newly added columns from pms.partner and pms.partner_h
+ALTER TABLE pms.partner DROP COLUMN IF EXISTS logo_url;
+ALTER TABLE pms.partner_h DROP COLUMN IF EXISTS logo_url;
+ALTER TABLE pms.partner DROP COLUMN IF EXISTS addl_info;
+ALTER TABLE pms.partner_h DROP COLUMN IF EXISTS addl_info;
+
+-- Drop the newly added column from pms.misp_license
+ALTER TABLE pms.misp_license DROP COLUMN IF EXISTS policy_id;
+
+-- Drop the newly created pms.oidc_client table
+DROP TABLE IF EXISTS pms.oidc_client CASCADE;
