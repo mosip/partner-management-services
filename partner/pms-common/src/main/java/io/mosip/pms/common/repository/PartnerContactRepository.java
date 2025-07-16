37d2d0d6f14fb1deae21dd982b148ddc82d0f814
sudeep <sudeep.j7353@gmail.com>
2025-07-16 10:04:38 +0530

    MOSIP-41491:Security Testing: PMS Legacy - DB has PII issues - Create utility to encrypt existing data in DB
    Signed-off-by: sudeep <sudeep.j7353@gmail.com>

--- partner/pms-common/src/main/java/io/mosip/pms/common/repository/PartnerContactRepository.java
+++ partner/pms-common/src/main/java/io/mosip/pms/common/repository/PartnerContactRepository.java
@@ -6,6 +6,8 @@ import org.springframework.stereotype.Repository;
 
 import io.mosip.pms.common.entity.PartnerContact;
 
+import java.util.List;
+
 @Repository
 public interface PartnerContactRepository extends JpaRepository<PartnerContact, String>{
 	
@@ -14,4 +16,7 @@ public interface PartnerContactRepository extends JpaRepository<PartnerContact, 
 
 	@Query(value = "select * from partner_contact pc where pc.partner_id=?1 and pc.email_id = ?2", nativeQuery = true )
 	public PartnerContact findByPartnerAndEmailId(String partnerId, String emailId);
+
+	@Query(value = "SELECT * FROM partner_contact pc WHERE pc.email_id_hash IS NULL", nativeQuery = true)
+	List<PartnerContact> findAllWithNullEmailIdHash();
 } 
--- partner/pms-common/src/main/java/io/mosip/pms/common/repository/PartnerHRepository.java
+++ partner/pms-common/src/main/java/io/mosip/pms/common/repository/PartnerHRepository.java
@@ -1,11 +1,18 @@
 package io.mosip.pms.common.repository;
 
+import io.mosip.pms.common.entity.Partner;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import io.mosip.pms.common.entity.PartnerH;
 
+import java.util.List;
+
 @Repository
 public interface PartnerHRepository extends JpaRepository<PartnerH, String>{
+
+    @Query(value = "SELECT * FROM partner_h p WHERE p.email_id_hash IS NULL", nativeQuery = true)
+    List<PartnerH> findPartnersWithNullEmailHash();
 
 }
