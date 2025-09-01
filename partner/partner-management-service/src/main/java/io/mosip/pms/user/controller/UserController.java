4fc16fad80d5debb9f12bf3465538c9b4a10cf11
sudeep <sudeep.j7353@gmail.com>
2025-09-01 18:24:33 +0530

    MOSIP-33105: Added POST /partners/v3 endpoint to support misp partner creation by partner admin.
    Signed-off-by: sudeep <sudeep.j7353@gmail.com>

--- partner/partner-management-service/src/main/java/io/mosip/pms/user/controller/UserController.java
+++ partner/partner-management-service/src/main/java/io/mosip/pms/user/controller/UserController.java
@@ -72,8 +72,8 @@ public class UserController {
 	@Value("${mosip.pms.api.id.users.notifications.seen.timestamp.put}")
 	private String putNotificationsSeenTimestampId;
 
-	@Value("${mosip.pms.oidc.supported.languages}")
-	private String oidcSupportedLanguages;
+	@Value("${mosip.pms.supported.notification.languages}")
+	private String supportedNotificationLanguages;
 
 	public static final String VERSION = "1.0";
 
@@ -137,7 +137,7 @@ public class UserController {
 		configMap.put("isOidcClientAvailable", isOidcClientAvailable);
 		configMap.put("isRootIntermediateCertAvailable", isRootIntermediateCertAvailable);
 		configMap.put("refreshNotificationsTime", refreshNotificationsTime);
-		configMap.put("oidcSupportedLanguages", oidcSupportedLanguages);
+		configMap.put("supportedNotificationLanguages", supportedNotificationLanguages);
 		responseWrapper.setResponse(configMap);
 		System.out.println(responseWrapper);
 		return responseWrapper;
