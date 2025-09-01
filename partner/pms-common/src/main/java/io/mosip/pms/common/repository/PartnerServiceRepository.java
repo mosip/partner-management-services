20677f8ddcab2feaab1a6dd5fe1ba1bcdb47272a
Swetha K <swetha.k@technoforte.co.in>
2025-09-01 17:55:38 +0530

    MOSIP-42461: Updated GET /partners/v3 for MISP_Partner
    Signed-off-by: Swetha K <swetha.k@technoforte.co.in>

--- partner/pms-common/src/main/java/io/mosip/pms/common/repository/PartnerServiceRepository.java
+++ partner/pms-common/src/main/java/io/mosip/pms/common/repository/PartnerServiceRepository.java
@@ -56,14 +56,10 @@ public interface PartnerServiceRepository extends JpaRepository<Partner, String>
 
     @Query("SELECT p FROM Partner p " +
             "WHERE p.approvalStatus = :status " +
-            "AND (:partnerType IS NULL OR p.partnerTypeCode = :partnerType) " +
-            "AND ((:policyGroupAvailable IS NULL) " +
-            "OR (:policyGroupAvailable = TRUE AND p.policyGroupId IS NOT NULL) " +
-            "OR (:policyGroupAvailable = FALSE AND p.policyGroupId IS NULL))")
-    public List<Partner> findPartnersByStatusAndPartnerTypeAndPolicyGroupAvailable(
+            "AND (:partnerType IS NULL OR p.partnerTypeCode = :partnerType)")
+    public List<Partner> findPartnersByStatusAndPartnerType(
             @Param("status") String status,
-            @Param("partnerType") String partnerType,
-            @Param("policyGroupAvailable") Boolean policyGroupAvailable);
+            @Param("partnerType") String partnerType);
     
     @Query(value = "select * from partner ppr where (ppr.is_deleted is null or ppr.is_deleted = false) and ppr.is_active = true", nativeQuery = true)
     public List<Partner> findAllByIsDeletedFalseorIsDeletedIsNullAndIsActiveTrue();
