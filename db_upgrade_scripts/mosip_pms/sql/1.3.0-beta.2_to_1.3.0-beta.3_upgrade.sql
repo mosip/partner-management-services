93946a444f67df7a1378c84bc802d7fe7042ca87
Swetha K <swetha.k@technoforte.co.in>
2025-09-12 10:49:59 +0530

    MOSIP-42478: created unique index in misp_license table
    Signed-off-by: Swetha K <swetha.k@technoforte.co.in>

--- db_upgrade_scripts/mosip_pms/sql/1.3.0-beta.2_to_1.3.0-beta.3_upgrade.sql
+++ db_upgrade_scripts/mosip_pms/sql/1.3.0-beta.2_to_1.3.0-beta.3_upgrade.sql
@@ -1,5 +1,8 @@
 \c mosip_pms
 
+-- Add new column for license_key_name in misp_license table
+ALTER TABLE pms.misp_license ADD COLUMN license_key_name character varying(128);
+
 -- Creating unique index for policy_id and license_key_name
 CREATE UNIQUE INDEX IF NOT EXISTS uq_policy_id_license_key_name
 ON pms.misp_license (COALESCE(policy_id, 'N/A'), license_key_name);
