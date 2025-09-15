a5878ec79349449cf92fe2d26db1bb14f02ba016
Swetha K <swetha.k@technoforte.co.in>
2025-09-15 12:09:56 +0530

    MOSIP-42478: add license_key_name col in misp_license table
    Signed-off-by: Swetha K <swetha.k@technoforte.co.in>

--- db_upgrade_scripts/mosip_pms/sql/1.3.0-beta.2_to_1.3.0-beta.3_rollback.sql
+++ db_upgrade_scripts/mosip_pms/sql/1.3.0-beta.2_to_1.3.0-beta.3_rollback.sql
@@ -1,7 +1,4 @@
 \c mosip_pms
 
--- Dropping unique index from pms.misp_license table if it exists
-DROP INDEX IF EXISTS uq_policy_id_license_key_name;
-
 -- Rollback script for the license_key_name column in misp_license table
 ALTER TABLE pms.misp_license DROP COLUMN license_key_name;
