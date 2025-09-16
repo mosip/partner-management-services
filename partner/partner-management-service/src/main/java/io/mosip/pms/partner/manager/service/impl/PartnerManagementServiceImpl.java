dfe26b5bf0a66e2a4590aa0eadb2b37ccb366dc9
sudeep <sudeep.j7353@gmail.com>
2025-09-16 13:37:31 +0530

    MOSIP-41187, MOSIP-42861: Added policy group name in response dto and added support in /admin-partners/{partnerId} for MISP_Partner.
    Signed-off-by: sudeep <sudeep.j7353@gmail.com>

--- partner/partner-management-service/src/main/java/io/mosip/pms/partner/manager/service/impl/PartnerManagementServiceImpl.java
+++ partner/partner-management-service/src/main/java/io/mosip/pms/partner/manager/service/impl/PartnerManagementServiceImpl.java
@@ -849,6 +849,7 @@ public class PartnerManagementServiceImpl implements PartnerManagerService {
 					isEncrypted ? keyManagerHelper.decryptData(partner.getEmailId()) : partner.getEmailId());
 			if ((!partner.getPartnerTypeCode().equals(FTM_PROVIDER) &&
 					!partner.getPartnerTypeCode().equals(DEVICE_PROVIDER) &&
+					!partner.getPartnerTypeCode().equals(MISP_PARTNER) &&
 					(Objects.isNull(partner.getPolicyGroupId()) || partner.getPolicyGroupId().isEmpty()))) {
 				LOGGER.info("sessionId", "idType", "id",
 						"Policy Group Id is empty for partner Id -" + partner.getId());
@@ -1316,6 +1317,7 @@ public class PartnerManagementServiceImpl implements PartnerManagerService {
 			LinkPolicyGroupResponseDto responseDto = new LinkPolicyGroupResponseDto();
 			responseDto.setPartnerId(partner.getId());
 			responseDto.setPolicyGroupId(partner.getPolicyGroupId());
+			responseDto.setPolicyGroupName(policyGroup.getName());
 
 			responseWrapper.setResponse(responseDto);
 		} catch (PartnerServiceException ex) {
--- partner/partner-management-service/src/main/java/io/mosip/pms/partner/request/dto/LinkPolicyGroupResponseDto.java
+++ partner/partner-management-service/src/main/java/io/mosip/pms/partner/request/dto/LinkPolicyGroupResponseDto.java
@@ -6,4 +6,5 @@ import lombok.Data;
 public class LinkPolicyGroupResponseDto {
     private String partnerId;
     private String policyGroupId;
+    private String policyGroupName;
 }
