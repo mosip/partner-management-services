eff96d5d6018af0099f2d5529cbc43715c40e773
sudeep <sudeep.j7353@gmail.com>
2025-10-08 16:29:16 +0530

    MOSIP-41992, MOSIP-41990: Added NOT NULL constraint for partner.email column and corrected cr_by in reg_device and reg_device_subtype table.
    Signed-off-by: sudeep <sudeep.j7353@gmail.com>

--- db_upgrade_scripts/mosip_pms/sql/1.3.0-beta.2_to_1.3.0-beta.3_rollback.sql
+++ db_upgrade_scripts/mosip_pms/sql/1.3.0-beta.2_to_1.3.0-beta.3_rollback.sql
@@ -2,3 +2,7 @@
 
 -- Rollback script for the license_key_name column in misp_license table
 ALTER TABLE pms.misp_license DROP COLUMN license_key_name;
+
+-- Remove NOT NULL constraint from email_id
+ALTER TABLE pms.partner
+    ALTER COLUMN email_id DROP NOT NULL;
