be0377e172ee2ed2db5aea3c91cb9485b506bf62
sudeep <sudeep.j7353@gmail.com>
2025-10-08 16:42:27 +0530

    MOSIP-41992, MOSIP-41990: Added NOT NULL constraint for partner.email column and corrected cr_by in reg_device and reg_device_subtype table.
    Signed-off-by: sudeep <sudeep.j7353@gmail.com>

--- db_upgrade_scripts/mosip_pms/sql/1.3.0-beta.2_to_1.3.0-beta.3_upgrade.sql
+++ db_upgrade_scripts/mosip_pms/sql/1.3.0-beta.2_to_1.3.0-beta.3_upgrade.sql
@@ -2,3 +2,15 @@
 
 -- Add new column for license_key_name in misp_license table
 ALTER TABLE pms.misp_license ADD COLUMN license_key_name character varying(128);
+
+-- Add NOT NULL constraint to email_id
+ALTER TABLE pms.partner
+    ALTER COLUMN email_id SET NOT NULL;
+
+-- Update all records in pms.reg_device_sub_type to set cr_by = 'superadmin'
+UPDATE pms.reg_device_sub_type
+SET cr_by = 'superadmin';
+
+-- Update all records in pms.reg_device_type to set cr_by = 'superadmin'
+UPDATE pms.reg_device_type
+SET cr_by = 'superadmin';
