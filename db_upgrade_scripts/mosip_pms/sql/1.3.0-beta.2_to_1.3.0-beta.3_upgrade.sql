39e8f01340d46d0d8c78934a9020f5017ca9b0bc
Sudeep7353 <103890430+Sudeep7353@users.noreply.github.com>
2025-10-10 13:13:44 +0530

    MOSIP-41988, MOSIP-43297: database cleanup, deprecated old endpoints (#1534)
    * MOSIP-41992, MOSIP-41990: Added NOT NULL constraint for partner.email column and corrected cr_by in reg_device and reg_device_subtype table.
    
    Signed-off-by: sudeep <sudeep.j7353@gmail.com>
    
    * MOSIP-41992, MOSIP-41990: Added NOT NULL constraint for partner.email column and corrected cr_by in reg_device and reg_device_subtype table.
    
    Signed-off-by: sudeep <sudeep.j7353@gmail.com>
    
    ---------
    
    Signed-off-by: sudeep <sudeep.j7353@gmail.com>

--- db_upgrade_scripts/mosip_pms/sql/1.3.0-beta.2_to_1.3.0-beta.3_upgrade.sql
+++ db_upgrade_scripts/mosip_pms/sql/1.3.0-beta.2_to_1.3.0-beta.3_upgrade.sql
@@ -2,3 +2,18 @@
 
 -- Add new column for license_key_name in misp_license table
 ALTER TABLE pms.misp_license ADD COLUMN license_key_name character varying(128);
+
+-- Add NOT NULL constraint to email_id
+ALTER TABLE pms.partner
+    ALTER COLUMN email_id SET NOT NULL;
+
+-- Update all records in pms.reg_device_sub_type to set cr_by = 'superadmin'
+UPDATE pms.reg_device_sub_type
+SET cr_by = 'superadmin';
+
+-- Update all records in pms.reg_device_type to set cr_by = 'superadmin'
+UPDATE pms.reg_device_type
+SET cr_by = 'superadmin';
+
+-- Drop the otp_transaction
+DROP TABLE IF EXISTS pms.otp_transaction CASCADE;
