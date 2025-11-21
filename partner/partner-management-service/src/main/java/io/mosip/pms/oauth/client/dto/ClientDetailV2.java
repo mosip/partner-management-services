afece3069ef2824831a6fb6f77895d8345891a81
SwethaKrish4 <110164892+SwethaKrish4@users.noreply.github.com>
2025-11-21 11:14:06 +0530

    MOSIP-43553: updated GET /oidc-clients/{clientId} (#1591)
    * MOSIP-43553: updated GET /oidc-clients/{clientId}
    
    Signed-off-by: Swetha K <swetha.k@technoforte.co.in>
    
    * MOSIP-43553: updated GET /oidc-clients/{clientId}
    
    Signed-off-by: Swetha K <swetha.k@technoforte.co.in>
    
    * MOSIP-43553: updated GET /oidc-clients/{clientId}
    
    Signed-off-by: Swetha K <swetha.k@technoforte.co.in>
    
    ---------
    
    Signed-off-by: Swetha K <swetha.k@technoforte.co.in>
    Co-authored-by: Swetha K <swetha.k@technoforte.co.in>

--- partner/partner-management-service/src/main/java/io/mosip/pms/oauth/client/dto/ClientDetailV2.java
+++ partner/partner-management-service/src/main/java/io/mosip/pms/oauth/client/dto/ClientDetailV2.java
@@ -9,8 +9,10 @@ import java.util.Map;
 @Data
 public class ClientDetailV2 extends ClientDetail {
 
+    private String policyGroupName;
+    private String policyGroupDescription;
+    private String policyDescription;
     private Map<String, String> clientNameLangMap;
-
     private AdditionalConfigDto additionalConfig;
 
 }
