65439fef4326d6acdeab1aaab96902e1c26890fe
Swetha K <swetha.k@technoforte.co.in>
2025-12-11 12:47:59 +0530

    MOSIP-43657: Backward Compatibility for OIDC Client Creation and Update
    Signed-off-by: Swetha K <swetha.k@technoforte.co.in>

--- partner/partner-management-service/src/main/java/io/mosip/pms/user/controller/UserController.java
+++ partner/partner-management-service/src/main/java/io/mosip/pms/user/controller/UserController.java
@@ -84,6 +84,9 @@ public class UserController {
 	@Value("${mosip.pms.supported.oidc.languages}")
 	private String supportedOidcLanguages;
 
+	@Value("${mosip.pms.oidc.client.additional.info.required}")
+	private String isOidcClientAdditionalInfoRequired;
+
 	public static final String VERSION = "1.0";
 
 	@Autowired
@@ -150,6 +153,7 @@ public class UserController {
 		configMap.put("partnerIdMaxLength", partnerIdMaxLength);
 		configMap.put("phoneNumberMaxLength", phoneNumberMaxLength);
 		configMap.put("supportedOidcLanguages", supportedOidcLanguages);
+		configMap.put("isOidcClientAdditionalInfoRequired", isOidcClientAdditionalInfoRequired);
 		responseWrapper.setResponse(configMap);
 		System.out.println(responseWrapper);
 		return responseWrapper;
