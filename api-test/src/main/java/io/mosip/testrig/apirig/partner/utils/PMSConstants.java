4998d9655696c5cc0fbf3df6c76c10f9be03157f
SradhaMohanty5899 <mohantysradha10@gmail.com>
2025-12-22 15:58:52 +0530

    MOSIP-43659
    Signed-off-by: SradhaMohanty5899 <mohantysradha10@gmail.com>

--- api-test/src/main/java/io/mosip/testrig/apirig/partner/utils/PMSConstants.java
+++ api-test/src/main/java/io/mosip/testrig/apirig/partner/utils/PMSConstants.java
@@ -5,4 +5,5 @@ import io.mosip.testrig.apirig.utils.GlobalConstants;
 public class PMSConstants extends GlobalConstants {
 
 	public static final String FEATURE_NOT_SUPPORTED_PMSREVAMP = "feature not supported. in the current version of MOSIP Platform. Hence skipping the testcase";
+	public static final String FEATURE_NOT_SUPPORTED_PMSREVAMP_FOR_OIDC = "OIDC client feature not supported for eSignet versions below 1.6.2. Hence skipping the testcase";
 }
