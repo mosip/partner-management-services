1d3f0af91aba1398c86494834dde895afe874db2
Chetan Kumar Hirematha <chetankumar.h.239@gmail.com>
2026-01-05 11:22:29 +0530

    [MOSIP-43915] : Corrected grammar in error message.
    Signed-off-by: Chetan Kumar Hirematha <chetankumar.h.239@gmail.com>

--- partner/partner-management-service/src/main/java/io/mosip/pms/partner/constant/ErrorCode.java
+++ partner/partner-management-service/src/main/java/io/mosip/pms/partner/constant/ErrorCode.java
@@ -34,7 +34,7 @@ public enum ErrorCode {
 	CREDENTIAL_NOT_ALLOWED_PARTNERS("PMS_PRT_070","Credential mapping allowed only for :"),
 	POLICY_PARSING_ERROR("PMS_PRT_071","Error occured while parsing policy string to json object"),
 	CREDENTIAL_TYPE_NOT_ALLOWED("PMS_PRT_072","Given credential type is not allowed. Allowed credential types : "),
-	POLICY_NOT_EXIST("PMS_PRT_073","Policy not exists."),
+	POLICY_NOT_EXIST("PMS_PRT_073","Policy does not exists."),
 	PARTNER_POLICY_TYPE_MISMATCH("PMS_PRT_057","Policy Type Mismatch. Only Auth policy can be used to create OIDC Client."),	
 	EMAIL_EXISTS_IN_KEYCLOAK("PMS_PRT_074","User exists with same email(keycloak)"),
 	PARTNER_DOES_NOT_EXIST_EXCEPTION("PMS_PRT_005","Partner does not exist"),
