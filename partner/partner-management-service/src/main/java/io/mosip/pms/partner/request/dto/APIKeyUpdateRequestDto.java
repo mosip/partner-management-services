116edd9931d0a7855aeead920bee80b0870cebd6
Sudeep7353 <103890430+Sudeep7353@users.noreply.github.com>
2026-01-08 18:37:35 +0530

    MOSIP-44155 : Updated request for PATCH /partners/{partnerId}/policies/{policyId}/api-keys/{apiKeyName} (#1637)
    Signed-off-by: sudeep j <sudeepj@sudeeps-MacBook-Pro.local>
    Co-authored-by: sudeep j <sudeepj@sudeeps-MacBook-Pro.local>

--- partner/partner-management-service/src/main/java/io/mosip/pms/partner/request/dto/APIKeyUpdateRequestDto.java
+++ partner/partner-management-service/src/main/java/io/mosip/pms/partner/request/dto/APIKeyUpdateRequestDto.java
@@ -8,7 +8,7 @@ import java.time.OffsetDateTime;
 @Data
 public class APIKeyUpdateRequestDto {
 
-	@Schema(description = "Status of the API key. Use 'INACTIVE' to deactivate the API key", example = "INACTIVE")
+	@Schema(description = "Status of the API key. Use 'De-active' to deactivate the API key", example = "De-active")
 	private String status;
 	
 	@Schema(description = "Expiry date and time for the API key", example = "2024-12-31T23:59:59.000Z")
