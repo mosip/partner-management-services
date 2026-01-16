f58b658092a299e0a89379b15966e92a786335c9
Chetan Kumar Hirematha <chetankumar.h.239@gmail.com>
2026-01-16 14:11:42 +0530

    MOSIP-44105: Added an annotation @url for redirect uri validation (#1643)
    * MOSIP-44105: Added an annotation @url for redirect uri validation
    
    Signed-off-by: Chetan Kumar Hirematha <chetankumar.h.239@gmail.com>
    
    * MOSIP-44105: Added an annotation @url for redirect uri validation
    
    Signed-off-by: Chetan Kumar Hirematha <chetankumar.h.239@gmail.com>
    
    * MOSIP-44105: Added an annotation @url for redirect uri validation
    
    Signed-off-by: Chetan Kumar Hirematha <chetankumar.h.239@gmail.com>
    
    ---------
    
    Signed-off-by: Chetan Kumar Hirematha <chetankumar.h.239@gmail.com>

--- partner/partner-management-service/src/main/java/io/mosip/pms/oauth/client/dto/CreateClientRequestDto.java
+++ partner/partner-management-service/src/main/java/io/mosip/pms/oauth/client/dto/CreateClientRequestDto.java
@@ -10,6 +10,7 @@ import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.URL;
 
 @Data
 @AllArgsConstructor
@@ -21,8 +22,9 @@ public class CreateClientRequestDto {
     private String relyingPartyId;
     private List<String> userClaims;
     private List<String> authContextRefs;
+    @URL
     private String logoUri;
-    private List<String> redirectUris;
+    private List<@URL String> redirectUris;
     private List<String> grantTypes;
     private List<String> clientAuthMethods;
  
