aeb2ca248aab71c4b3c9b88e5046e9690acb27a8
Chetan Kumar Hirematha <chetankumar.h.239@gmail.com>
2026-02-17 13:30:57 +0530

    added junit test cases
    Signed-off-by: Chetan Kumar Hirematha <chetankumar.h.239@gmail.com>

--- partner/partner-management-service/src/test/java/io/mosip/pms/test/partner/controller/PartnerServiceControllerTest.java
+++ partner/partner-management-service/src/test/java/io/mosip/pms/test/partner/controller/PartnerServiceControllerTest.java
@@ -353,6 +353,8 @@ public class PartnerServiceControllerTest {
         requestWrapper.setRequest(requestDto);
         requestWrapper.setId("mosip.pms.partner.generateapikey");
         requestWrapper.setVersion("1.0");
+        requestWrapper.setRequesttime(java.time.LocalDateTime.now());
+        when(requestValidator.validate(anyString(), any())).thenReturn(Optional.empty());
         when(partnerManagerService.generateAPIKey(eq(partnerId), eq(policyId), any(GenerateAPIKeyRequestDto.class)))
                 .thenReturn(responseWrapper);
         mockMvc.perform(MockMvcRequestBuilders.post("/partners/" + partnerId + "/policies/" + policyId + "/api-keys")
@@ -381,6 +383,8 @@ public class PartnerServiceControllerTest {
         requestWrapper.setRequest(requestDto);
         requestWrapper.setId("mosip.pms.partner.generateapikey");
         requestWrapper.setVersion("1.0");
+        requestWrapper.setRequesttime(java.time.LocalDateTime.now());
+        when(requestValidator.validate(anyString(), any())).thenReturn(Optional.empty());
         when(partnerManagerService.generateAPIKey(eq(partnerId), eq(policyId), any(GenerateAPIKeyRequestDto.class)))
                 .thenReturn(responseWrapper);
         mockMvc.perform(MockMvcRequestBuilders.post("/partners/" + partnerId + "/policies/" + policyId + "/api-keys")
