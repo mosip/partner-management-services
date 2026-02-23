4788a61a747aa9c16856e7033614bacef9f609c1
Dhanendra Sahu <dhanendra.tech@gmail.com>
2026-02-23 12:27:08 +0530

    Updated type of share from direct to data share for PDFCard
    Signed-off-by: Dhanendra Sahu <dhanendra.tech@gmail.com>

---
+++ db_upgrade_scripts/mosip_pms/sql/1.3.0-beta.4_to_1.3.0-beta.5_rollback.sql
@@ -0,0 +1,30 @@
+\c mosip_pms
+
+-- Rollback: Revert typeOfShare from "Data Share" back to "direct"
+
+UPDATE pms.auth_policy
+SET policy_file_id = REPLACE(policy_file_id, '"typeOfShare":"Data Share"', '"typeOfShare":"direct"')
+WHERE id='mpolicy-default-eUIN_with_faceQR';
+
+
+UPDATE pms.auth_policy
+SET policy_file_id = REPLACE(policy_file_id, '"typeOfShare":"Data Share"', '"typeOfShare":"direct"')
+WHERE id='mpolicy-default-eUIN_with_QR';
+
+
+UPDATE pms.auth_policy_h
+SET policy_file_id = REPLACE(policy_file_id, '"typeOfShare":"Data Share"', '"typeOfShare":"direct"')
+WHERE id='mpolicy-default-eUIN_with_faceQR'
+AND eff_dtimes='2020-11-13 05:58:00.000';
+
+
+UPDATE pms.auth_policy_h
+SET policy_file_id = REPLACE(policy_file_id, '"typeOfShare":"Data Share"', '"typeOfShare":"direct"')
+WHERE id='mpolicy-default-eUIN_with_QR'
+AND eff_dtimes='2020-11-13 05:58:00.000';
+
+
+UPDATE pms.auth_policy_h
+SET policy_file_id = REPLACE(policy_file_id, '"typeOfShare":"Data Share"', '"typeOfShare":"direct"')
+WHERE id='mpolicy-default-PDFCard'
+AND eff_dtimes='2023-11-14 05:59:00.000';
