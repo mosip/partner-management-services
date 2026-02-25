67b6b7313e8a24c42df94c6c6c0d0b36ae2df9a8
Chetan Kumar Hirematha <chetankumar.h.239@gmail.com>
2026-02-25 10:56:56 +0530

    [MOSIP-44438] : Updated GET/partner-api-keys/v2 endpoint to include partnerType
    Signed-off-by: Chetan Kumar Hirematha <chetankumar.h.239@gmail.com>

--- partner/partner-management-service/src/main/java/io/mosip/pms/partner/manager/controller/PartnerManagementController.java
+++ partner/partner-management-service/src/main/java/io/mosip/pms/partner/manager/controller/PartnerManagementController.java
@@ -560,7 +560,7 @@ public class PartnerManagementController {
 		if (status != null) filterDto.setStatus(status);
 		if (policyName != null) filterDto.setPolicyName(policyName.toLowerCase());
 		if (policyGroupName != null) filterDto.setPolicyGroupName(policyGroupName.toLowerCase());
-		if (partnerType != null) filterDto.setPartnerType(partnerType.toLowerCase());
+		if (partnerType != null && !partnerType.isBlank()) filterDto.setPartnerType(partnerType.toLowerCase());
 		if (expiryPeriod != null) filterDto.setExpiryPeriod(expiryPeriod);
 		return filterDto;
 	}
