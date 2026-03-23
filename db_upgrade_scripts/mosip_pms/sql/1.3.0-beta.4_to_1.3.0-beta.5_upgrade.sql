f190a51f8c6bc4f6ab8f6bac9239de7c6f7e6b7a
Chetan Kumar Hirematha <chetankumar.h.239@gmail.com>
2026-03-23 11:19:56 +0530

    [MOSIP-44673] : Created API : POST: /bio-extractor-configurations & table: bioextractor_configuration
    Signed-off-by: Chetan Kumar Hirematha <chetankumar.h.239@gmail.com>

--- db_upgrade_scripts/mosip_pms/sql/1.3.0-beta.4_to_1.3.0-beta.5_upgrade.sql
+++ db_upgrade_scripts/mosip_pms/sql/1.3.0-beta.4_to_1.3.0-beta.5_upgrade.sql
@@ -27,3 +27,15 @@ UPDATE pms.auth_policy_h
 SET policy_file_id = REPLACE(policy_file_id, '"typeOfShare":"direct"', '"typeOfShare":"Data Share"')
 WHERE id='mpolicy-default-PDFCard'
 AND eff_dtimes='2023-11-14 05:59:00.000';
+
+CREATE TABLE IF NOT EXISTS pms.bioextractor_configuration(
+                                                             id character varying(36) NOT NULL,
+    config_name character varying(128) NOT NULL,
+    bioextractor_provider_name character varying(128) NOT NULL,
+    bioextractor_provider_version character varying(36),
+    bio_modality character varying(64) NOT NULL,
+    cr_by character varying(256) NOT NULL,
+    cr_dtimes timestamp NOT NULL,
+    CONSTRAINT pk_bioextractor_configuration PRIMARY KEY (id),
+    CONSTRAINT uq_bioextractor_configuration_config_name UNIQUE (config_name)
+    );
--- db_upgrade_scripts/mosip_pms/sql/1.3.0-beta.5_to_1.3.0-beta.6_rollback.sql
+++
@@ -1,4 +0,0 @@
-\c mosip_pms
-
--- Rollback: Drop the bioextractor_configuration table
-DROP TABLE IF EXISTS pms.bioextractor_configuration;
