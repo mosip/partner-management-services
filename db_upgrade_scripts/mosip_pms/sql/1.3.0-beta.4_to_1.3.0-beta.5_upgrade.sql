41352bc9ca874c55b98a259796cce9e635e4534b
Chetan Kumar Hirematha <chetankumar.h.239@gmail.com>
2026-04-07 18:34:36 +0530

    Change extractor_provider_version length to 8 characters
    Signed-off-by: Chetan Kumar Hirematha <chetankumar.h.239@gmail.com>

--- db_upgrade_scripts/mosip_pms/sql/1.3.0-beta.4_to_1.3.0-beta.5_upgrade.sql
+++ db_upgrade_scripts/mosip_pms/sql/1.3.0-beta.4_to_1.3.0-beta.5_upgrade.sql
@@ -51,7 +51,7 @@ CREATE TABLE IF NOT EXISTS pms.partner_policy_bioextract_request (
     policy_id character varying(36) NOT NULL,
     attribute_name character varying(128) NOT NULL,
     extractor_provider character varying(128) NOT NULL,
-    extractor_provider_version character varying(36),
+    extractor_provider_version character varying(8),
     biometric_modality character varying(64) NOT NULL,
     biometric_sub_types character varying(64),
     status_code character varying(20) NOT NULL DEFAULT 'InProgress',
