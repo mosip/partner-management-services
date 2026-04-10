883257b0b03c35a68a2fd5d5db77a2757a6da5ea
Chetan Kumar Hirematha <chetankumar.h.239@gmail.com>
2026-04-10 11:50:31 +0530

    comments addressed
    Signed-off-by: Chetan Kumar Hirematha <chetankumar.h.239@gmail.com>

--- partner/pms-common/src/main/java/io/mosip/pms/common/repository/PartnerPolicyCredentialTypeRepository.java
+++ partner/pms-common/src/main/java/io/mosip/pms/common/repository/PartnerPolicyCredentialTypeRepository.java
@@ -13,6 +13,6 @@ public interface PartnerPolicyCredentialTypeRepository extends JpaRepository<Par
 	@Query(value = "select * from partner_policy_credential_type pc where pc.part_id =?1 and pc.credential_type =?2 ",nativeQuery = true)
 	PartnerPolicyCredentialType findByPartnerIdAndCrdentialType(String partnerId,String credentialType);
 
-	@Query(value = "select * from partner_policy_credential_type pc where pc.part_id =?1 and pc.policy_id =?2 and pc.is_active=true", nativeQuery = true)
+	@Query(value = "select * from partner_policy_credential_type pc where pc.part_id =?1 and pc.policy_id =?2 and (pc.is_deleted is null or pc.is_deleted = false) and pc.is_active=true",nativeQuery = true)
 	List<PartnerPolicyCredentialType> findByPartnerIdAndPolicyIdAndIsActiveTrue(String partnerId, String policyId);
 }
