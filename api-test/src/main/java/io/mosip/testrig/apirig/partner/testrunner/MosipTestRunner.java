f51c541520908d16c4e0e45dc964747257922fdc
Mohanachandran S <mohanachandran.s@technoforte.co.in>
2026-04-28 20:17:45 +0530

    MOSIP-44958 - Pushed the latest changes to release-1.3.x branches for 1.3.0-beta.5 release
    Signed-off-by: Mohanachandran S <mohanachandran.s@technoforte.co.in>

--- api-test/src/main/java/io/mosip/testrig/apirig/partner/testrunner/MosipTestRunner.java
+++ api-test/src/main/java/io/mosip/testrig/apirig/partner/testrunner/MosipTestRunner.java
@@ -82,7 +82,7 @@ public class MosipTestRunner {
 			setLogLevels();
 
 			HealthChecker healthcheck = new HealthChecker();
-			healthcheck.setCurrentRunningModule(BaseTestCase.currentModule);
+			healthcheck.setCurrentRunningModule(GlobalConstants.PARTNER_MANAGEMENT_SERVICE);
 			Thread trigger = new Thread(healthcheck);
 			trigger.start();
 
@@ -119,7 +119,7 @@ public class MosipTestRunner {
 		// Used for generating the test case interdependency JSON file
 		if ("yes".equalsIgnoreCase(generateDependency)) {
 			LOGGER.info("Generating test case inter-dependencies");
-			AdminTestUtil.generateTestCaseInterDependencies(BaseTestCase.testCaseInterDependencyPath);
+			AdminTestUtil.generateTestCaseInterDependencies(BaseTestCase.getTestCaseInterDependencyPath());
 		} else {
 			LOGGER.info("Skipping dependency generation");
 		}
--- api-test/src/main/java/io/mosip/testrig/apirig/partner/testscripts/DownloadRootCertificate.java
+++ api-test/src/main/java/io/mosip/testrig/apirig/partner/testscripts/DownloadRootCertificate.java
@@ -1,6 +1,5 @@
 package io.mosip.testrig.apirig.partner.testscripts;
 
-import java.lang.reflect.Field;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -18,8 +17,6 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
-import org.testng.internal.BaseTestMethod;
-import org.testng.internal.TestResult;
 
 import io.mosip.testrig.apirig.dto.OutputValidationDto;
 import io.mosip.testrig.apirig.dto.TestCaseDTO;
@@ -27,7 +24,6 @@ import io.mosip.testrig.apirig.partner.utils.PMSConfigManager;
 import io.mosip.testrig.apirig.partner.utils.PMSUtil;
 import io.mosip.testrig.apirig.testrunner.HealthChecker;
 import io.mosip.testrig.apirig.utils.AdminTestException;
-import io.mosip.testrig.apirig.utils.AdminTestUtil;
 import io.mosip.testrig.apirig.utils.AuthenticationTestException;
 import io.mosip.testrig.apirig.utils.GlobalConstants;
 import io.mosip.testrig.apirig.utils.OutputValidationUtil;
@@ -147,16 +143,6 @@ public class DownloadRootCertificate extends PMSUtil implements ITest {
 	 */
 	@AfterMethod(alwaysRun = true)
 	public void setResultTestName(ITestResult result) {
-		try {
-			Field method = TestResult.class.getDeclaredField("m_method");
-			method.setAccessible(true);
-			method.set(result, result.getMethod().clone());
-			BaseTestMethod baseTestMethod = (BaseTestMethod) result.getMethod();
-			Field f = baseTestMethod.getClass().getSuperclass().getDeclaredField("m_methodName");
-			f.setAccessible(true);
-			f.set(baseTestMethod, testCaseName);
-		} catch (Exception e) {
-			Reporter.log("Exception : " + e.getMessage());
-		}
+		result.setAttribute("TestCaseName", testCaseName);
 	}
 }
