58854f5086ac7adcbc9eaeb6508510a97882a9b5
Chetan Kumar Hirematha <chetankumar.h.239@gmail.com>
2026-04-29 15:03:32 +0530

    [MOSIP-44939] : Added validations of maximum length for input fields
    Signed-off-by: Chetan Kumar Hirematha <chetankumar.h.239@gmail.com>

--- partner/partner-management-service/src/main/java/io/mosip/pms/partner/request/dto/BioextractorConfigurationRequestDto.java
+++ partner/partner-management-service/src/main/java/io/mosip/pms/partner/request/dto/BioextractorConfigurationRequestDto.java
@@ -1,15 +1,20 @@
 package io.mosip.pms.partner.request.dto;
 
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
 public class BioextractorConfigurationRequestDto {
 
+	@Size(max = 126, message = "configName length must be <= 126")
 	private String configName;
 
+	@Size(max = 126, message = "Bio extractor provider name length must be <= 126")
 	private String bioextractorProviderName;
 
+	@Size(max = 36, message = "Bio extractor provider version length must be <= 36")
 	private String bioextractorProviderVersion;
 
+	@Size(max = 64, message = "Bio Modality length must be <= 64")
 	private String bioModality;
 }
