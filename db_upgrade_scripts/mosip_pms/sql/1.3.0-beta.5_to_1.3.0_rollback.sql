f194176dfa38c37fe5768ba261e4cdf70d44a1f7
Chetan Kumar Hirematha <chetankumar.h.239@gmail.com>
2026-06-29 18:18:45 +0530

    #1937, #1941, #1957 : Attribute name changed from individualbiometrics to photo, gave GRANT permission on table pms.user_details , Added declaredAsDeceased Attribute to Default Auth Policy for CRVS
    Signed-off-by: Chetan Kumar Hirematha <chetankumar.h.239@gmail.com>

--- db_upgrade_scripts/mosip_pms/sql/1.3.0-beta.5_to_1.3.0_rollback.sql
+++ db_upgrade_scripts/mosip_pms/sql/1.3.0-beta.5_to_1.3.0_rollback.sql
@@ -29,3 +29,13 @@ ALTER TABLE IF EXISTS pms.bioextractor_configuration
 ALTER TABLE IF EXISTS pms.bioextractor_configuration
     DROP COLUMN IF EXISTS is_deleted;
 
+-- -------------------------------------------------------------------------------------------------
+-- Rollback for CRVS attributes: remove declaredAsDeceased from mpolicy-default-auth (1.3.0)
+-- -------------------------------------------------------------------------------------------------
+
+UPDATE pms.auth_policy
+SET policy_file_id='{"shareableAttributes":[{"attributeName":"fullName","source":[{"attribute":"fullName"}],"encrypted":true},{"attributeName":"dateOfBirth","source":[{"attribute":"dateOfBirth"}],"encrypted":true},{"attributeName":"gender","source":[{"attribute":"gender"}],"encrypted":true},{"attributeName":"phone","source":[{"attribute":"phone"}],"encrypted":true},{"attributeName":"email","source":[{"attribute":"email"}],"encrypted":true},{"attributeName":"addressLine1","source":[{"attribute":"addressLine1"}],"encrypted":true},{"attributeName":"addressLine2","source":[{"attribute":"addressLine2"}],"encrypted":true},{"attributeName":"addressLine3","source":[{"attribute":"addressLine3"}],"encrypted":true},{"attributeName":"region","source":[{"attribute":"region"}],"encrypted":true},{"attributeName":"province","source":[{"attribute":"province"}],"encrypted":true},{"attributeName":"city","source":[{"attribute":"city"}],"encrypted":true},{"attributeName":"postalCode","source":[{"attribute":"postalCode"}],"encrypted":true},{"attributeName":"individualBiometrics","group":"CBEFF","source":[{"attribute":"individualBiometrics"}],"encrypted":true,"format":"extraction"}],"dataSharePolicies":{"typeOfShare":"Data Share","validForInMinutes":"30","transactionsAllowed":"2","encryptionType":"Partner Based","shareDomain":"datashare.datashare","source":"ID Repository"}}',
+    upd_by='admin',
+    upd_dtimes=now()
+WHERE id='mpolicy-default-auth';
+
