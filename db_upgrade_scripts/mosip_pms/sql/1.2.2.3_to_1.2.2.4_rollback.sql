b8c0b3bb03731810d6fc1efc00438cb32a7d4532
Chetan Kumar Hirematha <chetankumar.h.239@gmail.com>
2026-06-30 00:39:37 +0530

    "CRVS attributes added in auth_policy"
    Signed-off-by: Chetan Kumar Hirematha <chetankumar.h.239@gmail.com>

--- db_upgrade_scripts/mosip_pms/sql/1.2.2.3_to_1.2.2.4_rollback.sql
+++ db_upgrade_scripts/mosip_pms/sql/1.2.2.3_to_1.2.2.4_rollback.sql
@@ -7,8 +7,7 @@ SET attribute_name='individualBiometrics',
     upd_dtimes=now()
 WHERE id='146098';
 
--- Table : pms.auth_policy
--- Reason: Rollback CRVS attributes (declaredAsDeceased) from mpolicy-default-auth
+-- Rollback declaredAsDeceased attribute from mpolicy-default-auth in pms.auth_policy
 UPDATE pms.auth_policy
 SET policy_file_id='{"shareableAttributes":[{"attributeName":"fullName","source":[{"attribute":"fullName"}],"encrypted":true},{"attributeName":"dateOfBirth","source":[{"attribute":"dateOfBirth"}],"encrypted":true},{"attributeName":"gender","source":[{"attribute":"gender"}],"encrypted":true},{"attributeName":"phone","source":[{"attribute":"phone"}],"encrypted":true},{"attributeName":"email","source":[{"attribute":"email"}],"encrypted":true},{"attributeName":"addressLine1","source":[{"attribute":"addressLine1"}],"encrypted":true},{"attributeName":"addressLine2","source":[{"attribute":"addressLine2"}],"encrypted":true},{"attributeName":"addressLine3","source":[{"attribute":"addressLine3"}],"encrypted":true},{"attributeName":"region","source":[{"attribute":"region"}],"encrypted":true},{"attributeName":"province","source":[{"attribute":"province"}],"encrypted":true},{"attributeName":"city","source":[{"attribute":"city"}],"encrypted":true},{"attributeName":"postalCode","source":[{"attribute":"postalCode"}],"encrypted":true},{"attributeName":"zone","source":[{"attribute":"zone"}],"encrypted":true},{"attributeName":"preferredLang","source":[{"attribute":"preferredLang"}],"encrypted":false},{"attributeName":"individualBiometrics","group":"CBEFF","source":[{"attribute":"individualBiometrics"}],"encrypted":true,"format":"extraction"}],"dataSharePolicies":{"typeOfShare":"Data Share","validForInMinutes":"30","transactionsAllowed":"2","encryptionType":"Partner Based","shareDomain":"datashare.datashare","source":"ID Repository"}}',
     upd_by='admin',
--- db_upgrade_scripts/mosip_pms/sql/1.2.2.3_to_1.2.2.4_upgrade.sql
+++ db_upgrade_scripts/mosip_pms/sql/1.2.2.3_to_1.2.2.4_upgrade.sql
@@ -7,11 +7,7 @@ SET attribute_name='photo',
     upd_dtimes=now()
 WHERE id='146098';
 
--- Table : pms.auth_policy
--- Reason: CRVS attributes needs to be added
--- Note: Before executing check the existing policy_file_id for id='mpolicy-default-auth'
--- Extra attributes added: {"attributeName":"declaredAsDeceased","source":[{"attribute":"declaredAsDeceased"}],"encrypted":true}
--- After execution restart: Regproc, Packet-Manager, PMS
+-- declaredAsDeceased attribute added to mpolicy-default-auth in pms.auth_policy for CRVS support
 UPDATE pms.auth_policy
 SET policy_file_id='{"shareableAttributes":[{"attributeName":"fullName","source":[{"attribute":"fullName"}],"encrypted":true},{"attributeName":"dateOfBirth","source":[{"attribute":"dateOfBirth"}],"encrypted":true},{"attributeName":"gender","source":[{"attribute":"gender"}],"encrypted":true},{"attributeName":"phone","source":[{"attribute":"phone"}],"encrypted":true},{"attributeName":"email","source":[{"attribute":"email"}],"encrypted":true},{"attributeName":"addressLine1","source":[{"attribute":"addressLine1"}],"encrypted":true},{"attributeName":"addressLine2","source":[{"attribute":"addressLine2"}],"encrypted":true},{"attributeName":"addressLine3","source":[{"attribute":"addressLine3"}],"encrypted":true},{"attributeName":"region","source":[{"attribute":"region"}],"encrypted":true},{"attributeName":"province","source":[{"attribute":"province"}],"encrypted":true},{"attributeName":"city","source":[{"attribute":"city"}],"encrypted":true},{"attributeName":"postalCode","source":[{"attribute":"postalCode"}],"encrypted":true},{"attributeName":"zone","source":[{"attribute":"zone"}],"encrypted":true},{"attributeName":"preferredLang","source":[{"attribute":"preferredLang"}],"encrypted":false},{"attributeName":"individualBiometrics","group":"CBEFF","source":[{"attribute":"individualBiometrics"}],"encrypted":true,"format":"extraction"},{"attributeName":"declaredAsDeceased","source":[{"attribute":"declaredAsDeceased"}],"encrypted":true}],"dataSharePolicies":{"typeOfShare":"Data Share","validForInMinutes":"30","transactionsAllowed":"2","encryptionType":"Partner Based","shareDomain":"datashare.datashare","source":"ID Repository"}}',
     upd_by='admin',
